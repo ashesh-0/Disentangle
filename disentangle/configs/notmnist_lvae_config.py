dbdccf16e32882945cf5920c17cf1b7100aa75af
ashesh <ashesh276@gmail.com>
2022-02-20 07:41:04 +0530

    config update

--- disentangle/configs/notmnist_lvae_config.py
+++ disentangle/configs/notmnist_lvae_config.py
@@ -30,7 +30,7 @@ def get_config():
     model = config.model
     model.model_type = ModelType.LadderVae
     model.z_dims = [64, 64]
-    model.blocks_per_layer = 3
+    model.blocks_per_layer = 1
     model.nonlin = 'elu'
     model.merge_type = 'residual'
     model.batchnorm = True
