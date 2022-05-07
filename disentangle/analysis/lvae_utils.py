6f6e142d7a9cad8949146c9004f93157af122055
ashesh <ashesh276@gmail.com>
2022-05-07 22:28:07 +0200

    fix in utils

--- disentangle/analysis/lvae_utils.py
+++ disentangle/analysis/lvae_utils.py
@@ -6,14 +6,14 @@ from disentangle.core.data_utils import crop_img_tensor
 
 def get_img_from_forward_output(out, model):
     recons_img = model.likelihood.parameter_net(out)
-    recons_img = recons_img * model.data_std[0] + model.data_mean[0]
+    recons_img = recons_img * model.data_std + model.data_mean
     return recons_img
 
 
 def get_z(img, model):
     with torch.no_grad():
         img = torch.Tensor(img[None]).cuda()
-        x_normalized = (img - model.data_mean[0]) / model.data_std[0]
+        x_normalized = model.normalize(img)
         recons_img_latent, td_data = model(x_normalized)
         q_mu = td_data['q_mu']
         recons_img = get_img_from_forward_output(recons_img_latent, model)
