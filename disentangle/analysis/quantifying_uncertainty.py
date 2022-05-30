515c055ac159aac92a46573d98dd56da943eeae0
ashesh <ashesh276@gmail.com>
2022-05-30 09:10:41 +0200

    config update

--- disentangle/analysis/quantifying_uncertainty.py
+++ disentangle/analysis/quantifying_uncertainty.py
@@ -2,13 +2,14 @@
 Here, we have functions which can be used to quantify uncertainty in the predictions.
 """
 import numpy as np
-from typing import List, Dict
+from typing import List
 import torch
 from disentangle.analysis.lvae_utils import get_img_from_forward_output
 from disentangle.utils import PSNR, RangeInvariantPsnr
+import pandas as pd
 
 
-def sample_images(model, dset, idx_list, sample_count: int = 5) -> Dict[str, dict]:
+def sample_images(model, dset, idx_list, sample_count: int = 5):
     output = {}
     with torch.no_grad():
         for img_idx in idx_list:
@@ -24,19 +25,21 @@ def sample_images(model, dset, idx_list, sample_count: int = 5) -> Dict[str, dic
     return output
 
 
-def compute_regionwise_metric_one_pair(data1, data2, metric_types: List[str], regionsize: int) -> Dict[str, np.ndarray]:
+def compute_regionwise_metric_one_pair(data1, data2, metric_types: List[str], regionsize: int):
     # ensure that we are working with a square
     assert data1.shape[-1] == data1.shape[-2]
     assert data1.shape == data2.shape
+    Nc = data1.shape[-3]
     Nh = data1.shape[-2] // regionsize
     Nw = data1.shape[-1] // regionsize
-    output = {mtype: np.zeros((Nh, Nw)) for mtype in metric_types}
+    output = {mtype: np.zeros((Nh, Nw, Nc)) for mtype in metric_types}
     for hidx in range(Nh):
         for widx in range(Nw):
             h = hidx * regionsize
             w = widx * regionsize
             d1 = data1[..., h:h + regionsize, w:w + regionsize]
             d2 = data2[..., h:h + regionsize, w:w + regionsize]
+            #             import pdb;pdb.set_trace()
             met_dic = _compute_metrics(d1, d2, metric_types)
             for mtype in metric_types:
                 output[mtype][hidx, widx] = met_dic[mtype]
@@ -44,23 +47,28 @@ def compute_regionwise_metric_one_pair(data1, data2, metric_types: List[str], re
     return output
 
 
-def _compute_metrics(data1, data2, metric_types: List[str]) -> Dict[str, float]:
+def _compute_metrics(data1, data2, metric_types: List[str]):
+    data1 = data1.reshape(len(data1), -1)
+    data2 = data2.reshape(len(data2), -1)
+
     output = {}
+    #     import pdb;pdb.set_trace()
     for metric_type in metric_types:
         assert metric_type in ['PSNR', 'RangeInvariantPsnr', 'MSE']
 
         if metric_type == 'MSE':
-            metric = np.mean((data1 - data2) ** 2)
+            metric = np.mean((data1 - data2) ** 2, axis=1)
         elif metric_type == 'PSNR':
-            metric = PSNR(data1, data2)
+            metric = np.array([PSNR(data1[0], data2[0]), PSNR(data1[1], data2[1])])
         elif metric_type == 'RangeInvariantPsnr':
-            metric = RangeInvariantPsnr(data1, data2)
+            metric = np.array([RangeInvariantPsnr(data1[0], data2[0]),
+                               RangeInvariantPsnr(data1[1], data2[1])])
         output[metric_type] = metric
     return output
 
 
-def compute_regionwise_metric(model, dset, idx_list: List[int], metric_types: List[str], regionsize: int = 64,
-                              sample_count: int = 5) -> Dict[int, dict]:
+def compute_regionwise_metric(model, dset, idx_list: List[int], metric_types, regionsize: int = 64,
+                              sample_count: int = 5):
     """
     This will get the prediction multiple times for each of the idx. It would then compute the pairswise metric
     between the predictions, that too on small regions. So, if the model is not sure about a certain region, it would simply
@@ -73,15 +81,18 @@ def compute_regionwise_metric(model, dset, idx_list: List[int], metric_types: Li
     output = {}
     sample_dict = sample_images(model, dset, idx_list, sample_count=sample_count)
     for img_idx in idx_list:
+        output[img_idx] = {}
         assert len(sample_dict[img_idx]['rec']) == sample_count
         rec_list = sample_dict[img_idx]['rec']
         for idx1 in range(sample_count):
-            output[idx1] = {}
+            output[img_idx][idx1] = {}
             # NOTE: we need to iterate starting from 0 and not from idx1 + 1 since not every metric is symmetric.
             # PSNR is definitely not.
             for idx2 in range(sample_count):
+
                 if idx1 == idx2:
                     continue
-                output[idx1][idx2] = compute_regionwise_metric_one_pair(rec_list[idx1], rec_list[idx2], metric_types,
-                                                                        regionsize)
+                output[img_idx][idx1][idx2] = compute_regionwise_metric_one_pair(rec_list[idx1], rec_list[idx2],
+                                                                                 metric_types,
+                                                                                 regionsize)
     return output
--- disentangle/configs/microscopy_multi_channel_lvae_critic_config.py
+++ disentangle/configs/microscopy_multi_channel_lvae_critic_config.py
@@ -12,7 +12,7 @@ from disentangle.core.sampler_type import SamplerType
 def get_config():
     config = get_default_config()
     data = config.data
-    data.image_size = 128
+    data.image_size = 256
     data.data_type = DataType.OptiMEM100_014
     data.channel_1 = 0
     data.channel_2 = 2
@@ -23,10 +23,11 @@ def get_config():
     # If this is set to true, then one mean and stdev is used for both channels. Otherwise, two different
     # meean and stdev are used.
     data.use_one_mu_std = True
+    data.train_aug_rotate = True
 
     loss = config.loss
     loss.loss_type = LossType.ElboWithCritic
-    loss.kl_weight = 0.01
+    loss.kl_weight = 0.005
     loss.kl_annealing = False
     loss.kl_annealtime = 10
     loss.kl_start = -1
@@ -51,7 +52,7 @@ def get_config():
     model.no_initial_downscaling = True
     model.analytical_kl = True
     model.mode_pred = False
-    model.var_clip_max = 5
+    model.var_clip_max = 8
     # Discriminator params
     model.critic = ml_collections.ConfigDict()
     model.critic.ndf = 64
@@ -61,12 +62,13 @@ def get_config():
 
     training = config.training
     training.lr = 0.001
-    training.lr_scheduler_patience = 1000
-    training.max_epochs = 20000
-    training.batch_size = 8
+    training.lr_scheduler_patience = 15
+    training.max_epochs = 200
+    training.batch_size = 4
     training.num_workers = 4
-    training.val_repeat_factor = 50
-    training.train_repeat_factor = 10
+    training.val_repeat_factor = None
+    training.train_repeat_factor = None
     training.val_fraction = 0.2
-    config.training.earlystop_patience = 5000
+    training.earlystop_patience = 100
+    training.precision = 16
     return config
