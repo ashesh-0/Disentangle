e6a3fb2dee0e9766e56bb55a9ce694cc2c255d35
ashesh <ashesh276@gmail.com>
2022-06-30 15:45:18 +0200

    added multiscale data loader

--- disentangle/data_loader/multi_channel_determ_tiff_dloader.py
+++ disentangle/data_loader/multi_channel_determ_tiff_dloader.py
@@ -18,12 +18,12 @@ class MultiChDeterministicTiffDloader:
                  enable_rotation_aug: bool = False,
                  use_one_mu_std=None):
         """
-        Here, an image is split into grids of size img_sz. 
+        Here, an image is split into grids of size img_sz.
         Args:
             repeat_factor: Since we are doing a random crop, repeat_factor is
                 given which can repeatedly sample from the same image. If self.N=12
                 and repeat_factor is 5, then index upto 12*5 = 60 is allowed.
-            use_one_mu_std: If this is set to true, then one mean and stdev is used 
+            use_one_mu_std: If this is set to true, then one mean and stdev is used
                 for both channels. Otherwise, two different meean and stdev are used.
 
         """
@@ -31,6 +31,7 @@ class MultiChDeterministicTiffDloader:
         self._fpath = fpath
 
         self._data = train_val_data(self._fpath, is_train, channel_1, channel_2, val_fraction=val_fraction)
+
         self._normalized_input = normalized_input
         max_val = np.quantile(self._data, 0.995)
         self._data[self._data > max_val] = max_val
@@ -61,8 +62,8 @@ class MultiChDeterministicTiffDloader:
             return img1, img2, {'h': [0, h], 'w': [0, w], 'hflip': False, 'wflip': False}
 
         h_start, w_start = self._get_deterministic_hw(index, h, w)
-        img1 = self._crop_img(img1, h_start, w_start, False, False)
-        img2 = self._crop_img(img2, h_start, w_start, False, False)
+        img1 = self._crop_flip_img(img1, h_start, w_start, False, False)
+        img2 = self._crop_flip_img(img2, h_start, w_start, False, False)
 
         return img1, img2, {
             'h': [h_start, h_start + self._img_sz],
@@ -71,8 +72,12 @@ class MultiChDeterministicTiffDloader:
             'wflip': False,
         }
 
-    def _crop_img(self, img: np.ndarray, h_start: int, w_start: int, h_flip: bool, w_flip: bool):
+    def _crop_img(self, img: np.ndarray, h_start: int, w_start: int):
         new_img = img[..., h_start:h_start + self._img_sz, w_start:w_start + self._img_sz]
+        return new_img
+
+    def _crop_flip_img(self, img: np.ndarray, h_start: int, w_start: int, h_flip: bool, w_flip: bool):
+        new_img = self._crop_img(img, h_start, w_start)
         if h_flip:
             new_img = new_img[..., ::-1, :]
         if w_flip:
@@ -82,7 +87,7 @@ class MultiChDeterministicTiffDloader:
 
     def _get_deterministic_hw(self, index: int, h: int, w: int):
         """
-        Random starting position for the crop for the img with index `index`.
+        Fixed starting position for the crop for the img with index `index`.
         """
         assert h == w
         factor = index // self.N
@@ -134,7 +139,7 @@ class MultiChDeterministicTiffDloader:
 
     def _get_img(self, index: int):
         """
-        Loads an image. 
+        Loads an image.
         Crops the image such that cropped image has content.
         """
         img1, img2 = self._load_img(index)
---
+++ disentangle/data_loader/multiscale_mc_tiff_dloader.py
@@ -0,0 +1,116 @@
+"""
+Here, the input image is of multiple resolutions. Target image is the same.
+"""
+from typing import List, Tuple, Union
+
+import numpy as np
+from skimage.transform import resize
+
+from disentangle.data_loader.multi_channel_determ_tiff_dloader import MultiChDeterministicTiffDloader
+
+
+class MultiScaleTiffDloader(MultiChDeterministicTiffDloader):
+    def __init__(self,
+                 img_sz: int,
+                 fpath: str,
+                 channel_1: int,
+                 channel_2: int,
+                 is_train: Union[None, bool] = None,
+                 val_fraction=None,
+                 normalized_input=None,
+                 enable_rotation_aug: bool = False,
+                 use_one_mu_std=None,
+                 num_scales: int = None,
+                 ):
+        """
+        Args:
+            num_scales: The number of resolutions at which we want the input. Note that the target is formed at the
+                        highest resolution.
+        """
+        super().__init__(img_sz,
+                         fpath,
+                         channel_1,
+                         channel_2,
+                         is_train=is_train,
+                         val_fraction=val_fraction,
+                         normalized_input=normalized_input,
+                         enable_rotation_aug=enable_rotation_aug,
+                         use_one_mu_std=use_one_mu_std)
+        self.num_scales = num_scales
+        assert self.num_scales is not None
+        self._scaled_data = [self._data]
+        assert isinstance(self.num_scales, int) and self.num_scales >= 1
+
+        for _ in range(1, self.num_scales):
+            shape = self._scaled_data[-1].shape
+            assert len(shape) == 4
+            new_shape = (shape[0], shape[1] // 2, shape[2] // 2, shape[3])
+            ds_data = resize(self._scaled_data[-1], new_shape)
+            self._scaled_data.append(ds_data)
+
+    def _load_scaled_img(self, scaled_index, index: int) -> Tuple[np.ndarray, np.ndarray]:
+        imgs = self._scaled_data[scaled_index][index % self.N]
+        return imgs[None, :, :, 0], imgs[None, :, :, 1]
+
+    def _crop_img(self, img: np.ndarray, h_start: int, w_start: int):
+        """
+        Here, h_start, w_start could be negative. That simply means we need to pick the content from 0. So,
+        the cropped image will be smaller than self._img_sz * self._img_sz
+        """
+        h_end = h_start + self._img_sz
+        w_end = w_start + self._img_sz
+        h_start = max(0, h_start)
+        w_start = max(0, w_start)
+        new_img = img[..., h_start:h_end, w_start:w_end]
+        return new_img
+
+    def _get_img(self, index: int):
+        """
+        Loads an image.
+        Crops the image such that cropped image has content.
+        """
+        img1, img2 = self._load_img(index)
+        assert self._img_sz is not None
+        h, w = img1.shape[-2:]
+        h_start, w_start = self._get_deterministic_hw(index, h, w)
+        img1_cropped = self._crop_flip_img(img1, h_start, w_start, False, False)
+        img2_cropped = self._crop_flip_img(img2, h_start, w_start, False, False)
+
+        h_center = h_start + self._img_sz // 2
+        w_center = w_start + self._img_sz // 2
+        img1_versions = [img1_cropped]
+        img2_versions = [img2_cropped]
+        for scale_idx in range(1, self.num_scales):
+            img1, img2 = self._load_scaled_img(scale_idx, index)
+            h_center = h_center // 2
+            w_center = w_center // 2
+            img1_padded = np.zeros_like(img1_versions[-1])
+            img2_padded = np.zeros_like(img2_versions[-1])
+            h_start = h_center - self._img_sz // 2
+            w_start = w_center - self._img_sz // 2
+            img1_cropped = self._crop_flip_img(img1, h_start, w_start, False, False)
+            img2_cropped = self._crop_flip_img(img2, h_start, w_start, False, False)
+
+            h_start = max(0, -h_start)
+            w_start = max(0, -w_start)
+            h_end = h_start + img1_cropped.shape[1]
+            w_end = w_start + img1_cropped.shape[2]
+            img1_padded[:, h_start:h_end, w_start:w_end] = img1_cropped
+            img2_padded[:, h_start:h_end, w_start:w_end] = img2_cropped
+
+            img1_versions.append(img1_padded)
+            img2_versions.append(img2_padded)
+
+        img1 = np.concatenate(img1_versions, axis=0)
+        img2 = np.concatenate(img2_versions, axis=0)
+        return img1, img2
+
+    def __getitem__(self, index: int):
+        img1, img2 = self._get_img(index)
+        assert self._enable_rotation is False
+        target = np.concatenate([img1[:1], img2[:1]], axis=0)
+        if self._normalized_input:
+            img1, img2 = self.normalize_img(img1, img2)
+
+        inp = (0.5 * img1 + 0.5 * img2).astype(np.float32)
+        return inp, target
