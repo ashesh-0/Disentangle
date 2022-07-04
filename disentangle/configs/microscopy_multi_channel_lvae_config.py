55e4eaa4c13797fe8c6a92a5b952e97bbf057da3
ashesh <ashesh276@gmail.com>
2022-07-04 12:16:05 +0200

    config update

--- disentangle/configs/microscopy_multi_channel_lvae_config.py
+++ disentangle/configs/microscopy_multi_channel_lvae_config.py
@@ -8,7 +8,7 @@ from disentangle.core.sampler_type import SamplerType
 def get_config():
     config = get_default_config()
     data = config.data
-    data.image_size = 32
+    data.image_size = 64
     data.data_type = DataType.OptiMEM100_014
     data.channel_1 = 0
     data.channel_2 = 2
@@ -21,7 +21,7 @@ def get_config():
     data.use_one_mu_std = True
     data.train_aug_rotate = False
     data.randomized_channels = False
-    data.multiscale_lowres_count = 5
+    data.multiscale_lowres_count = None
 
     loss = config.loss
     loss.loss_type = LossType.Elbo
@@ -36,7 +36,7 @@ def get_config():
 
     model = config.model
     model.model_type = ModelType.LadderVae
-    model.z_dims = [128, 128, 128, 128]
+    model.z_dims = [128, 128, 128]
     model.blocks_per_layer = 5
     model.nonlin = 'elu'
     model.merge_type = 'residual'
@@ -62,7 +62,7 @@ def get_config():
     training.lr = 0.0001
     training.lr_scheduler_patience = 15
     training.max_epochs = 200
-    training.batch_size = 16
+    training.batch_size = 4
     training.num_workers = 4
     training.val_repeat_factor = None
     training.train_repeat_factor = None
