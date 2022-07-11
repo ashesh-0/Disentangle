197d326bacae7a055c81ea14c97360bc360a6e47
ashesh <ashesh276@gmail.com>
2022-07-11 15:05:15 +0200

    config update

--- disentangle/configs/microscopy_multi_channel_lvae_config.py
+++ disentangle/configs/microscopy_multi_channel_lvae_config.py
@@ -59,10 +59,10 @@ def get_config():
     model.multiscale_lowres_separate_branch = False
 
     training = config.training
-    training.lr = 0.0001
+    training.lr = 0.001
     training.lr_scheduler_patience = 15
     training.max_epochs = 200
-    training.batch_size = 16
+    training.batch_size = 4
     training.num_workers = 4
     training.val_repeat_factor = None
     training.train_repeat_factor = None
