41f18629c8bda278566777a16d2f147f3b05e6af
ashesh <ashesh276@gmail.com>
2022-07-19 22:01:44 +0200

    config update

--- disentangle/configs/microscopy_multi_channel_lvae_config.py
+++ disentangle/configs/microscopy_multi_channel_lvae_config.py
@@ -36,13 +36,13 @@ def get_config():
 
     model = config.model
     model.model_type = ModelType.LadderVae
-    model.z_dims = [128, 128, 128, 128]
+    model.z_dims = [64, 64, 64, 64]
     model.blocks_per_layer = 1
     model.nonlin = 'elu'
     model.merge_type = 'residual'
     model.batchnorm = True
     model.stochastic_skip = True
-    model.n_filters = 64
+    model.n_filters = 32
     model.dropout = 0.1
     model.learn_top_prior = True
     model.img_shape = None
