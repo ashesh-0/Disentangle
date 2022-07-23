843c10494313d536e3fed2f36b6d75e5af179aaf
ashesh <ashesh276@gmail.com>
2022-07-23 12:12:49 +0200

    config update

--- disentangle/configs/microscopy_multi_channel_lvae_config.py
+++ disentangle/configs/microscopy_multi_channel_lvae_config.py
@@ -8,7 +8,7 @@ from disentangle.core.sampler_type import SamplerType
 def get_config():
     config = get_default_config()
     data = config.data
-    data.image_size = 32
+    data.image_size = 16
     data.data_type = DataType.OptiMEM100_014
     data.channel_1 = 0
     data.channel_2 = 2
@@ -66,7 +66,7 @@ def get_config():
     training.lr = 0.001
     training.lr_scheduler_patience = 15
     training.max_epochs = 200
-    training.batch_size = 32
+    training.batch_size = 64
     training.num_workers = 4
     training.val_repeat_factor = None
     training.train_repeat_factor = None
