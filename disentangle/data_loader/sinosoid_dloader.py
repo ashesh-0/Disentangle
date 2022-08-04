49d8de3db9ac610ea8be9407d143d0f46df92e55
Ashesh <ashesh276@gmail.com>
2022-08-04 18:41:56 +0200

    dataset getting created

---
+++ disentangle/data_loader/sinosoid_dloader.py
@@ -0,0 +1,194 @@
+import numpy as np
+import math
+from tqdm import tqdm
+
+
+def angle_shift(w1, w2, point):
+    """
+    Find x such that:    cos(w2*(point +x) = cos(w1*point)
+    """
+    # there should be two points at which the gradient's value should be same.
+    # if I select the correct point, then I don't need to shift
+    # d/dx(sin(w2*point +d)) = d/dx(sin(w1*point))
+    # w2*cos() = w1*cos()
+    assert w2 >= w1, 'w2 must be larger than w1. otherwise angle is not always possible'
+    theta = np.arccos(w1 * np.cos(w1 * point) / w2)
+    return theta - w2 * point
+
+
+def generate_one_curve(w1, w2, max_angle, granularity=0.1):
+    r1 = np.arange(0, max_angle // 2, granularity)
+    shift = angle_shift(w1, w2, r1[-1])
+    first_val = r1[-1] + shift / w2
+    r2 = np.arange(first_val, first_val + max_angle // 2, granularity)
+    lefthalf = np.sin(w1 * r1)
+    value_shift = np.sin(w1 * r1[-1]) - np.sin(w2 * r2[0])
+    righthalf = np.sin(w2 * r2) + value_shift
+
+    y = np.concatenate([lefthalf[:-1], righthalf])
+    x = np.concatenate([r1[:-1], r2 - shift / w2])
+    return y, x
+
+
+def apply_rotation(xy, radians):
+    """
+        Adapted from https://gist.github.com/LyleScott/e36e08bfb23b1f87af68c9051f985302
+        Args:
+            xy: (2,N)
+        """
+    c, s = np.cos(radians), np.sin(radians)
+    j = np.array([[c, -s], [s, c]])
+    m = np.dot(j, xy)
+    return np.array(m)
+
+
+def post_processing(x, curve, img_sz):
+    x = x.astype(np.int)
+    assert min(x) >= 0
+    x_filtr = x < img_sz
+    x = x[x_filtr]
+    curve = curve[x_filtr]
+    curve = curve.astype(np.int)
+    y_filtr = curve < img_sz
+
+    curve = curve[y_filtr]
+    x = x[y_filtr]
+    return x, curve
+
+
+def rotate_curve(x, curve, rotate_radian):
+    shift = (max(x) - min(x)) / 2
+    x = x - shift
+    x = x.reshape(1, -1)
+    curve = curve.reshape(1, -1)
+    xy = np.concatenate([x, curve], axis=0)
+    xy = apply_rotation(xy, rotate_radian)
+    x = xy[0] + shift
+    x = x - min(x)
+    curve = xy[1]
+    return x, curve
+
+
+def get_img(w_list, img_sz, vertical_shifts: list, rotate_radians: list, curve_amplitudes: list,
+            random_w12_flips: list):
+    assert len(vertical_shifts) == len(rotate_radians)
+    assert len(vertical_shifts) == len(curve_amplitudes)
+    img = np.zeros((img_sz, img_sz))
+    for i in range(len(w_list)):
+        w1, w2 = w_list[i]
+        add_to_img(img, w1, w2, vertical_shift=vertical_shifts[i], flip_about_vertical=random_w12_flips[i],
+                   rotate_radian=rotate_radians[i], curve_amplitude=curve_amplitudes[i])
+
+    return img
+
+
+def add_to_img(img, w1, w2, vertical_shift=0, flip_about_vertical=False, rotate_radian=0, curve_amplitude=64):
+    max_angle = img.shape[-1]
+    granularity = 0.1
+    curve, x = generate_one_curve(w1, w2, max_angle, granularity=granularity)
+    if flip_about_vertical:
+        min_x = min(x)
+        max_x = max(x)
+        x = min_x + (max_x - min_x) - (x - min_x)
+    curve = curve * curve_amplitude / (curve.max() - curve.min())
+    # positive
+    curve = curve - min(curve)
+    # vertical shift
+    curve += vertical_shift
+    if rotate_radian != 0:
+        x, curve = rotate_curve(x, curve, rotate_radian)
+
+    x, curve = post_processing(x, curve, img.shape[-1])
+    img[curve, x] += 1
+
+
+class Range:
+    def __init__(self, min_val, max_val):
+        assert min_val < max_val
+        self.min = min_val
+        self.max = max_val
+
+    def inrange(self, val):
+        return val >= self.min and val < self.max
+
+    def sample(self):
+        return np.random.rand() * (self.max - self.min) + self.min
+
+
+def sample_for_channel1(w_rangelist):
+    assert len(w_rangelist) == 4
+    if np.random.rand() > 0.5:
+        return w_rangelist[0].sample(), w_rangelist[2].sample()
+    else:
+        return w_rangelist[1].sample(), w_rangelist[3].sample()
+
+
+def sample_for_channel2(w_rangelist):
+    assert len(w_rangelist) == 4
+    if np.random.rand() > 0.5:
+        return w_rangelist[0].sample(), w_rangelist[3].sample()
+    else:
+        return w_rangelist[1].sample(), w_rangelist[2].sample()
+
+
+def generate_dataset(w_rangelist, size, img_sz, num_curves=3, curve_amplitude=64, max_rotation=math.pi / 8,
+                     flip_w12_randomly=False):
+    ch1_dset = []
+    ch2_dset = []
+
+    def sample_angle():
+        return 2 * np.random.rand() * max_rotation - max_rotation
+
+    def get_random_w12_flips():
+        if flip_w12_randomly:
+            random_w12_flips = [np.random.rand() > 0.5 for _ in range(num_curves)]
+        else:
+            random_w12_flips = [False] * num_curves
+        return random_w12_flips
+
+    for _ in tqdm(range(size)):
+        w1_list = [sample_for_channel1(w_rangelist) for _ in range(num_curves)]
+        w2_list = [sample_for_channel2(w_rangelist) for _ in range(num_curves)]
+        vertical_shifts = [np.random.rand() * img_sz for _ in range(num_curves)]
+        rotate_radians = [sample_angle() for _ in range(num_curves)]
+        img1 = get_img(w1_list, img_sz, vertical_shifts, rotate_radians, [curve_amplitude] * num_curves,
+                       get_random_w12_flips())
+        img2 = get_img(w2_list, img_sz, vertical_shifts, rotate_radians, [curve_amplitude] * num_curves,
+                       get_random_w12_flips())
+
+        ch1_dset.append(img1)
+        ch2_dset.append(img2)
+    return ch1_dset, ch2_dset
+
+
+if __name__ == '__main__':
+    w1 = 0.05
+    w2 = 0.15
+    max_angle = 100
+    # curve, x = generate_one_curve(w1, w2, max_angle)
+    # x = 2 * x / max_angle - 1
+    # # x = np.arange(len(curve))
+    # xy = np.concatenate([x.reshape(1, -1), curve.reshape(1, -1)], axis=0)
+    # rotated = apply_rotation(xy, math.pi / 200)
+    # print(curve.shape)
+    import matplotlib.pyplot as plt
+
+    # img = np.zeros((512, 512))
+    # vshift = np.random.rand() * img.shape[-1]
+    # max_rotate = math.pi / 8
+    # rotate = 2 * np.random.rand() * max_rotate - max_rotate
+    # add_to_img(img, w1, w2, vertical_shift=vshift, rotate_radian=rotate)
+    #
+    # vshift = np.random.rand() * img.shape[-1]
+    # rotate = 2 * np.random.rand() * max_rotate - max_rotate
+    # add_to_img(img, w1, w2, vertical_shift=vshift, rotate_radian=rotate)
+    # plt.imshow(img)
+    # plt.plot(x, curve)
+    # plt.plot(rotated[0], rotated[1], color='r')
+    w_rangelist = [Range(0.05, 0.1), Range(0.15, 0.2), Range(0.25, 0.3), Range(0.35, 0.4)]
+    size = 10
+    img_sz = 512
+    imgs1, imgs2 = generate_dataset(w_rangelist, size, img_sz, num_curves=3, curve_amplitude=64,
+                                    max_rotation=math.pi / 8, flip_w12_randomly=True)
+    plt.imshow(imgs1[0])
+    plt.show()
