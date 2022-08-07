deaac398b464dbc1394465bd1e1eff13ceb77aad
Ashesh <ashesh276@gmail.com>
2022-08-07 18:45:06 +0000

    config update

--- disentangle/configs/customdata_lvae_config.py
+++ disentangle/configs/customdata_lvae_config.py
@@ -28,7 +28,7 @@ def get_config():
     data.use_one_mu_std = True
     data.train_aug_rotate = False
     data.randomized_channels = False
-    data.multiscale_lowres_count = None
+    data.multiscale_lowres_count = 3
     data.padding_mode = 'constant'
     data.padding_value = 0
 
@@ -73,7 +73,7 @@ def get_config():
     training = config.training
     training.lr = 0.001
     training.lr_scheduler_patience = 15
-    training.max_epochs = 200
+    training.max_epochs = 400
     training.batch_size = 32
     training.num_workers = 4
     training.val_repeat_factor = None
