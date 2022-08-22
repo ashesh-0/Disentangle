d03f5aa33ab7ffe233143f92841ef1d173cd34b8
Ashesh <ashesh276@gmail.com>
2022-08-22 16:52:37 +0200

    config update

--- disentangle/configs/microscopy_multi_channel_lvae_config.py
+++ disentangle/configs/microscopy_multi_channel_lvae_config.py
@@ -21,7 +21,7 @@ def get_config():
     data.use_one_mu_std = True
     data.train_aug_rotate = False
     data.randomized_channels = False
-    data.multiscale_lowres_count = None
+    data.multiscale_lowres_count = 2
     data.padding_mode = 'reflect'
     data.padding_value = None
 
@@ -38,8 +38,8 @@ def get_config():
 
     model = config.model
     model.model_type = ModelType.LadderVae
-    model.z_dims = [128, 128, 128, 128]
-    model.blocks_per_layer = 1
+    model.z_dims = [128, 128]
+    model.blocks_per_layer = 5
     model.nonlin = 'elu'
     model.merge_type = 'residual'
     model.batchnorm = True
