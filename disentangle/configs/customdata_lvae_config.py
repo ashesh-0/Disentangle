379228d8fa31094455f281e93608a6961d9305d2
Ashesh <ashesh276@gmail.com>
2022-08-31 12:20:12 +0200

    config update

--- disentangle/configs/customdata_lvae_config.py
+++ disentangle/configs/customdata_lvae_config.py
@@ -29,7 +29,7 @@ def get_config():
     data.use_one_mu_std = True
     data.train_aug_rotate = False
     data.randomized_channels = False
-    data.multiscale_lowres_count = None
+    data.multiscale_lowres_count = 2
     data.padding_mode = 'constant'
     data.padding_value = 0
     data.encourage_non_overlap_single_channel = False
@@ -50,7 +50,7 @@ def get_config():
     loss.free_bits = 0.0
 
     model = config.model
-    model.model_type = ModelType.LadderVae
+    model.model_type = ModelType.LadderVAEMultiTarget
     model.z_dims = [128, 128, 128]
     model.blocks_per_layer = 3
     model.nonlin = 'elu'
