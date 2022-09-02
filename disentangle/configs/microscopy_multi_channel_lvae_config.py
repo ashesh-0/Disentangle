31e23aa71637a31d26f05f5e68463e3098c08658
Ashesh <ashesh276@gmail.com>
2022-09-02 17:20:30 +0200

    config update

--- disentangle/configs/microscopy_multi_channel_lvae_config.py
+++ disentangle/configs/microscopy_multi_channel_lvae_config.py
@@ -39,8 +39,8 @@ def get_config():
     model = config.model
     model.model_type = ModelType.LadderVae
     model.z_dims = [128, 128, 128, 128]
-    model.skip_bottom_layers_count = 2  # For how many bottom layers, do we not want to use the encoder's output.
-    model.skip_bottom_layers_count_patience = 1
+    model.skip_bottom_layers_count = 1  # For how many bottom layers, do we not want to use the encoder's output.
+    model.skip_bottom_layers_count_patience = 3
     model.blocks_per_layer = 1
     model.nonlin = 'elu'
     model.merge_type = 'residual'
