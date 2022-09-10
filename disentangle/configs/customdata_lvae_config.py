67c28b5f602843cf1193bfa0e7ee8cae5e0b04c3
Ashesh <ashesh276@gmail.com>
2022-09-10 11:57:37 +0200

    config update

--- disentangle/configs/customdata_lvae_config.py
+++ disentangle/configs/customdata_lvae_config.py
@@ -78,14 +78,14 @@ def get_config():
 
     training = config.training
     training.lr = 0.001
-    training.lr_scheduler_patience = 270
-    training.max_epochs = 7200
+    training.lr_scheduler_patience = 540
+    training.max_epochs = 14400
     training.batch_size = 32
     training.num_workers = 4
     training.val_repeat_factor = None
     training.train_repeat_factor = None
     training.val_fraction = 0.2
-    training.earlystop_patience = 1800
+    training.earlystop_patience = 3600
     training.precision = 16
 
     return config
