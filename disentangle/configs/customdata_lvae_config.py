0b1a6bb8dc2fe1e58c58bcd403e79513faa651e7
Ashesh <ashesh276@gmail.com>
2022-09-10 11:45:56 +0200

    config update

--- disentangle/configs/customdata_lvae_config.py
+++ disentangle/configs/customdata_lvae_config.py
@@ -35,7 +35,7 @@ def get_config():
     data.encourage_non_overlap_single_channel = False
     data.vertical_min_spacing = data.curve_amplitude * 2
     # 0.5 would mean that 50% of the points would be covered with the connecting w.
-    data.connecting_w_len = 0.4
+    data.connecting_w_len = 0.2
     data.curve_initial_phase = 0.0
 
     loss = config.loss
