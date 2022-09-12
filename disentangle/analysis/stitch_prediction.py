0fdafcbf8224229c22043ff551a0d3936fa2bf45
Ashesh <ashesh276@gmail.com>
2022-09-12 15:35:25 +0200

    code for stitching works now

--- disentangle/analysis/stitch_prediction.py
+++ disentangle/analysis/stitch_prediction.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+
 class PatchLocation:
     """
     Encapsulates t_idx and spatial location.
@@ -11,7 +12,7 @@ class PatchLocation:
         self.w_start, self.w_end = w_idx_range
 
 
-def _get_location(extra_padding,hwt, pred_h, pred_w):
+def _get_location(extra_padding, hwt, pred_h, pred_w):
     h_start, w_start, t_idx = hwt
     h_end = h_start + pred_h
 
@@ -21,7 +22,7 @@ def _get_location(extra_padding,hwt, pred_h, pred_w):
     w_end = w_start + pred_w
     w_start += extra_padding
     w_end -= extra_padding
-    return PatchLocation((h_start, h_end), (w_start, w_end),t_idx)
+    return PatchLocation((h_start, h_end), (w_start, w_end), t_idx)
 
 
 def get_location_from_idx(dset, dset_input_idx, pred_h, pred_w):
@@ -39,7 +40,8 @@ def get_location_from_idx(dset, dset_input_idx, pred_h, pred_w):
     """
     extra_padding = dset.per_side_overlap_pixelcount()
     htw = dset.hwt_from_idx(dset_input_idx)
-    return _get_location(extra_padding,htw,pred_h,pred_w)
+    return _get_location(extra_padding, htw, pred_h, pred_w)
+
 
 def set_skip_boundary_pixels_mask(mask, loc, skip_count):
     if skip_count == 0:
@@ -47,10 +49,10 @@ def set_skip_boundary_pixels_mask(mask, loc, skip_count):
     assert skip_count > 0
     assert loc.h_end - skip_count >= 0
     assert loc.w_end - skip_count >= 0
-    mask[loc.t, loc.h_start:loc.h_start + skip_count, loc.w_start:loc.w_end] = False
-    mask[loc.t, loc.h_end - skip_count:loc.h_end, loc.w_start:loc.w_end] = False
-    mask[loc.t, loc.h_start:loc.h_end, loc.w_start:loc.w_start + skip_count] = False
-    mask[loc.t, loc.h_start:loc.h_end, loc.w_end - skip_count:loc.w_end] = False
+    mask[loc.t, :, loc.h_start:loc.h_start + skip_count, loc.w_start:loc.w_end] = False
+    mask[loc.t, :, loc.h_end - skip_count:loc.h_end, loc.w_start:loc.w_end] = False
+    mask[loc.t, :, loc.h_start:loc.h_end, loc.w_start:loc.w_start + skip_count] = False
+    mask[loc.t, :, loc.h_start:loc.h_end, loc.w_end - skip_count:loc.w_end] = False
 
 
 def set_skip_central_pixels_mask(mask, loc, skip_count):
@@ -61,7 +63,7 @@ def set_skip_central_pixels_mask(mask, loc, skip_count):
     w_mid = (loc.w_start + loc.w_end) // 2
     l_skip = skip_count // 2
     r_skip = skip_count - l_skip
-    mask[loc.t, h_mid - l_skip:h_mid + r_skip, w_mid - l_skip:w_mid + r_skip] = False
+    mask[loc.t, :, h_mid - l_skip:h_mid + r_skip, w_mid - l_skip:w_mid + r_skip] = False
 
 
 def stitched_prediction_mask(dset, padded_patch_shape, skip_boundary_pixel_count, skip_central_pixel_count):
@@ -75,12 +77,18 @@ def stitched_prediction_mask(dset, padded_patch_shape, skip_boundary_pixel_count
 
     Returns:
     """
-    mask = np.full(dset._data.shape, True)
+    N, H, W, C = dset._data.shape
+    mask = np.full((N, C, H, W), True)
     hN, wN = padded_patch_shape
     for dset_input_idx in range(len(dset)):
         loc = get_location_from_idx(dset, dset_input_idx, hN, wN)
         set_skip_boundary_pixels_mask(mask, loc, skip_boundary_pixel_count)
         set_skip_central_pixels_mask(mask, loc, skip_central_pixel_count)
+
+    old_img_sz = dset.get_img_sz()
+    dset.set_img_sz(dset._img_sz_for_hw)
+    mask = stitch_predictions(mask, dset)
+    dset.set_img_sz(old_img_sz)
     return mask
 
 
@@ -100,15 +108,16 @@ def stitch_predictions(predictions, dset):
 
     return output
 
+
 if __name__ == '__main__':
     extra_padding = 0
-    hwt1 = (0,0,0)
+    hwt1 = (0, 0, 0)
     pred_h = 4
     pred_w = 4
-    hwt2  = (pred_h,pred_w,2)
+    hwt2 = (pred_h, pred_w, 2)
     loc1 = _get_location(extra_padding, hwt1, pred_h, pred_w)
     loc2 = _get_location(extra_padding, hwt2, pred_h, pred_w)
-    mask = np.full((10,8,8),1)
+    mask = np.full((10, 8, 8), 1)
     set_skip_boundary_pixels_mask(mask, loc1, 1)
     set_skip_boundary_pixels_mask(mask, loc2, 1)
     print(mask[hwt1[-1]])
