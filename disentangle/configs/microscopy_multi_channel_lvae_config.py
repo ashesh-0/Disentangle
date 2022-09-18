af746f7ec4801da1d3662efe8c6d471c83b7d924
Ashesh <ashesh276@gmail.com>
2022-09-18 14:32:15 +0200

    config update

--- disentangle/configs/microscopy_multi_channel_lvae_config.py
+++ disentangle/configs/microscopy_multi_channel_lvae_config.py
@@ -8,7 +8,7 @@ from disentangle.core.sampler_type import SamplerType
 def get_config():
     config = get_default_config()
     data = config.data
-    data.image_size = 128
+    data.image_size = 64
     data.data_type = DataType.OptiMEM100_014
     data.channel_1 = 0
     data.channel_2 = 2
@@ -39,7 +39,7 @@ def get_config():
     model = config.model
     model.model_type = ModelType.LadderVae
     model.z_dims = [128, 128, 128, 128]
-    model.blocks_per_layer = 1
+    model.blocks_per_layer = 5
     model.nonlin = 'elu'
     model.merge_type = 'residual'
     model.batchnorm = True
