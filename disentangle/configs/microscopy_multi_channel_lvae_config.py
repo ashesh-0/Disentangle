6c2c6a31bbad8ecd8ce0a51a730df8a060abc3a0
ashesh <ashesh276@gmail.com>
2022-10-04 16:15:12 +0000

    added notimplemented error

--- disentangle/configs/microscopy_multi_channel_lvae_config.py
+++ disentangle/configs/microscopy_multi_channel_lvae_config.py
@@ -18,12 +18,15 @@ def get_config():
     data.normalized_input = True
     # If this is set to true, then one mean and stdev is used for both channels. Otherwise, two different
     # meean and stdev are used.
-    data.use_one_mu_std = False
+    data.use_one_mu_std = True
     data.train_aug_rotate = False
     data.randomized_channels = False
     data.multiscale_lowres_count = None
     data.padding_mode = 'reflect'
     data.padding_value = None
+    # If this is set to True, then target channels will be normalized from their separate mean.
+    # otherwise, target will be normalized just the same way as the input, which is determined by use_one_mu_std
+    data.target_separate_normalization = True
 
     loss = config.loss
     loss.loss_type = LossType.Elbo
