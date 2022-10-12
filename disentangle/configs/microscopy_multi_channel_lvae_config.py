6114133dafbc6f47fc39ad7ec0bb5809c664f788
ashesh <ashesh276@gmail.com>
2022-10-12 09:05:35 +0000

    config update

--- disentangle/configs/microscopy_multi_channel_lvae_config.py
+++ disentangle/configs/microscopy_multi_channel_lvae_config.py
@@ -9,7 +9,7 @@ def get_config():
     config = get_default_config()
     data = config.data
     data.image_size = 64
-    data.data_type = DataType.OptiMEM100_014
+    data.data_type = DataType.Prevedel_EMBL
     data.channel_1 = 0
     data.channel_2 = 2
     data.sampler_type = SamplerType.DefaultSampler
