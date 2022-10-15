1bec7636022c8076df9b20933d6ee55af88201c5
Ashesh <ashesh276@gmail.com>
2022-10-15 23:06:43 +0200

    config update

--- disentangle/configs/allencell_config.py
+++ disentangle/configs/allencell_config.py
@@ -74,7 +74,7 @@ def get_config():
     model.no_initial_downscaling = True
     model.analytical_kl = False
     model.mode_pred = False
-    model.var_clip_max = 20
+    model.var_clip_max = 100
     # predict_logvar takes one of the three values: [None,'global','channelwise','pixelwise']
     model.predict_logvar = 'global'
     model.logvar_lowerbound = -10  # -2.49 is log(1/12), from paper "Re-parametrizing VAE for stablity."
