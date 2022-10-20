9e3e453d739113b309af3e2d72a694bfb8ddddd6
ashesh <ashesh276@gmail.com>
2022-10-20 15:53:49 +0000

    config update

--- disentangle/configs/pembl_config.py
+++ disentangle/configs/pembl_config.py
@@ -82,7 +82,7 @@ def get_config():
     training = config.training
     training.lr = 0.001
     training.lr_scheduler_patience = 15
-    training.max_epochs = 200
+    training.max_epochs = 800
     training.batch_size = 32
     training.num_workers = 4
     training.val_repeat_factor = None
