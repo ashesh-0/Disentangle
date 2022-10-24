c917b0536224c76e2d5599c0ed218739a149e227
Ashesh <ashesh276@gmail.com>
2022-10-24 20:01:28 +0200

    config update

--- disentangle/nets/noise_model.py
+++ disentangle/nets/noise_model.py
@@ -1,4 +1,5 @@
 from disentangle.nets.hist_noise_model import HistNoiseModel
+from disentangle.nets.gmm_noise_model import GaussianMixtureNoiseModel
 import numpy as np
 import os
 import torch.nn as nn
@@ -7,10 +8,11 @@ import torch
 
 class DisentNoiseModel(nn.Module):
 
-    def __init__(self, hist1, hist2):
+    def __init__(self, n1model, n2model):
         super().__init__()
-        self.n1model = HistNoiseModel(hist1)
-        self.n2model = HistNoiseModel(hist2)
+        import pdb;pdb.set_trace()
+        self.n1model = n1model
+        self.n2model = n2model
 
     def likelihood(self, obs, signal):
         ll1 = self.n1model.likelihood(obs[:, :1], signal[:, :1])
@@ -20,7 +22,15 @@ class DisentNoiseModel(nn.Module):
 
 def get_noise_model(data_fpath, model_config):
     if 'enable_noise_model' in model_config and model_config.enable_noise_model:
-        hist1 = np.load(model_config.noise_model_ch1_fpath)
-        hist2 = np.load(model_config.noise_model_ch2_fpath)
-        return DisentNoiseModel(hist1, hist2)
+        print(f'Noise model Ch1: {model_config.noise_model_ch1_fpath}')
+        print(f'Noise model Ch2: {model_config.noise_model_ch2_fpath}')
+        if model_config.noise_model_type == 'hist':
+            hist1 = np.load(model_config.noise_model_ch1_fpath)
+            nmodel1 = HistNoiseModel(hist1)
+            hist2 = np.load(model_config.noise_model_ch2_fpath)
+            nmodel2 = HistNoiseModel(hist2)
+        elif model_config.noise_model_type =='gmm':
+            nmodel1 = GaussianMixtureNoiseModel(params = np.load(model_config.noise_model_ch1_fpath))
+            nmodel2 = GaussianMixtureNoiseModel(params = np.load(model_config.noise_model_ch2_fpath))
+        return DisentNoiseModel(nmodel1, nmodel2)
     return None
