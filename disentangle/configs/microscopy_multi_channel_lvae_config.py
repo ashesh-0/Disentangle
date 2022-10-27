101e62a40fc4cded80a1b05ffdc0bd87b07356a8
ashesh <ashesh276@gmail.com>
2022-10-27 08:22:43 +0000

    config update

--- disentangle/configs/microscopy_multi_channel_lvae_config.py
+++ disentangle/configs/microscopy_multi_channel_lvae_config.py
@@ -72,8 +72,8 @@ def get_config():
     model.analytical_kl = False
     model.mode_pred = False
     model.var_clip_max = 20
-    # predict_logvar takes one of the three values: [None,'global','channelwise','pixelwise']
-    model.predict_logvar = None
+    # predict_logvar takes one of the four values: [None,'global','channelwise','pixelwise']
+    model.predict_logvar = 'pixelwise'
     model.logvar_lowerbound = -5  # -2.49 is log(1/12), from paper "Re-parametrizing VAE for stablity."
     model.multiscale_lowres_separate_branch = False
     model.multiscale_retain_spatial_dims = True
