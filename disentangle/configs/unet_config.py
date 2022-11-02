bdf0a2a5400ced23c08e69087bbef0d2838004c9
Ashesh <ashesh276@gmail.com>
2022-11-03 00:34:32 +0100

    config update

--- disentangle/configs/unet_config.py
+++ disentangle/configs/unet_config.py
@@ -8,7 +8,7 @@ from disentangle.core.sampler_type import SamplerType
 def get_config():
     config = get_default_config()
     data = config.data
-    data.image_size = 64
+    data.image_size = 512
     data.data_type = DataType.OptiMEM100_014
     data.channel_1 = 0
     data.channel_2 = 2
@@ -36,14 +36,14 @@ def get_config():
 
     model = config.model
     model.model_type = ModelType.UNet
-    model.n_levels = 2
+    model.n_levels = 4
     model.monitor = 'val_psnr'
 
     training = config.training
     training.lr = 0.001
     training.lr_scheduler_patience = 15
     training.max_epochs = 200
-    training.batch_size = 32
+    training.batch_size = 16
     training.num_workers = 4
     training.val_repeat_factor = None
     training.train_repeat_factor = None
