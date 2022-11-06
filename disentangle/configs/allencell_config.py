5c0ca85fc93c02515fa028e9167d890171a923cd
ashesh <ashesh276@gmail.com>
2022-11-06 10:32:30 +0000

    allencell config

--- disentangle/configs/allencell_config.py
+++ disentangle/configs/allencell_config.py
@@ -8,7 +8,7 @@ from disentangle.core.sampler_type import SamplerType
 def get_config():
     config = get_default_config()
     data = config.data
-    data.image_size = 32
+    data.image_size = 64
     data.data_type = DataType.AllenCellMito
     data.channel_1 = 1
     data.channel_2 = 2
@@ -26,7 +26,7 @@ def get_config():
     data.use_one_mu_std = True
     data.train_aug_rotate = False
     data.randomized_channels = False
-    data.multiscale_lowres_count = 4
+    data.multiscale_lowres_count = 5
     data.padding_mode = 'reflect'
     data.padding_value = None
     # If this is set to True, then target channels will be normalized from their separate mean.
@@ -74,19 +74,20 @@ def get_config():
     model.no_initial_downscaling = True
     model.analytical_kl = False
     model.mode_pred = False
-    model.var_clip_max = 1000
-    # predict_logvar takes one of the three values: [None,'global','channelwise','pixelwise']
+    model.var_clip_max = 20
+    # predict_logvar takes one of the four values: [None,'global','channelwise','pixelwise']
     model.predict_logvar = 'pixelwise'
-    model.logvar_lowerbound = -10  # -2.49 is log(1/12), from paper "Re-parametrizing VAE for stablity."
+    model.logvar_lowerbound = -5  # -2.49 is log(1/12), from paper "Re-parametrizing VAE for stablity."
     model.multiscale_lowres_separate_branch = False
     model.multiscale_retain_spatial_dims = True
     model.monitor = 'val_psnr'  # {'val_loss','val_psnr'}
+    model.non_stochastic_version = False
 
     training = config.training
     training.lr = 0.001
     training.lr_scheduler_patience = 15
     training.max_epochs = 200
-    training.batch_size = 64
+    training.batch_size = 32
     training.num_workers = 4
     training.val_repeat_factor = None
     training.train_repeat_factor = None
