384b0cbbfb4e21bed9f4e9b1931b4e2b3a8e01a5
ashesh <ashesh276@gmail.com>
2022-11-07 00:23:01 +0000

    config update

--- disentangle/configs/unet_config.py
+++ disentangle/configs/unet_config.py
@@ -8,7 +8,7 @@ from disentangle.core.sampler_type import SamplerType
 def get_config():
     config = get_default_config()
     data = config.data
-    data.image_size = 128
+    data.image_size = 512
     data.data_type = DataType.OptiMEM100_014
     data.channel_1 = 0
     data.channel_2 = 2
@@ -41,15 +41,15 @@ def get_config():
 
     training = config.training
     training.lr = 0.001
-    training.lr_scheduler_patience = 30
-    training.max_epochs = 400
+    training.lr_scheduler_patience = 60
+    training.max_epochs = 800
     training.batch_size = 32
     training.num_workers = 4
     training.val_repeat_factor = None
     training.train_repeat_factor = None
     training.val_fraction = 0.1
     training.test_fraction = 0.1
-    training.earlystop_patience = 200
+    training.earlystop_patience = 400
     training.precision = 16
 
     return config
