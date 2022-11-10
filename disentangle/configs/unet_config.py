ee123e938163d945e979f8da5a9a6800f794d886
Ashesh <ashesh276@gmail.com>
2022-11-10 12:07:25 +0000

    config update

--- disentangle/configs/unet_config.py
+++ disentangle/configs/unet_config.py
@@ -10,7 +10,7 @@ def get_config():
     data = config.data
     data.image_size = 256
     data.data_type = DataType.OptiMEM100_014
-    data.channel_1 = 0
+    data.channel_1 = 2
     data.channel_2 = 3
 
     data.sampler_type = SamplerType.DefaultSampler
