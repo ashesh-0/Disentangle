6cd65a41d23135ea504dc2b3a111d9aae44ac5d7
Ashesh <ashesh276@gmail.com>
2022-11-10 10:30:14 +0000

    config update

--- disentangle/configs/twotiff_unet_config.py
+++ disentangle/configs/twotiff_unet_config.py
@@ -8,7 +8,7 @@ from disentangle.core.sampler_type import SamplerType
 def get_config():
     config = get_default_config()
     data = config.data
-    data.image_size = 512
+    data.image_size = 256
     data.data_type = DataType.SeparateTiffData
     data.channel_1 = 0
     data.channel_2 = 1
@@ -45,7 +45,7 @@ def get_config():
     training.lr = 0.001
     training.lr_scheduler_patience = 30
     training.max_epochs = 400
-    training.batch_size = 8
+    training.batch_size = 32
     training.num_workers = 4
     training.val_repeat_factor = None
     training.train_repeat_factor = None
