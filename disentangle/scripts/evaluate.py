04a763a165fe64dea4a6556c128a3cec9cee18c0
Ashesh <ashesh276@gmail.com>
2022-11-14 22:42:00 +0100

    config update

--- disentangle/scripts/evaluate.py
+++ disentangle/scripts/evaluate.py
@@ -447,15 +447,12 @@ if __name__ == '__main__':
     DEBUG = False
     OUTPUT_DIR = os.path.expanduser('~/data/paper_stats/')
     ckpt_dirs = [
-        '/home/ashesh.ashesh/training/disentangle/2211/D3-M10-S0-L3/6',
-        '/home/ashesh.ashesh/training/disentangle/2211/D3-M10-S0-L3/9',
-        '/home/ashesh.ashesh/training/disentangle/2211/D3-M10-S0-L3/15',
-        '/home/ashesh.ashesh/training/disentangle/2211/D3-M10-S0-L3/1',
+        '/home/ashesh.ashesh/training/disentangle/2211/D3-M10-S0-L3/12',
     ]
     ckpt_dirs = [x[:-1] if '/' == x[-1] else x for x in ckpt_dirs]
     mmse_count = 1
 
-    for custom_image_size in [512]:
+    for custom_image_size in [256]:
         for eval_datasplit_type in [DataSplitType.Test]:
             for ckpt_dir in ckpt_dirs:
                 for image_size_for_grid_centers in [128]:
