7ceb37f82f25338512f0a35e82eccaeba46dd07e
Ashesh <ashesh276@gmail.com>
2022-11-15 09:42:30 +0100

    config update

--- disentangle/scripts/evaluate.py
+++ disentangle/scripts/evaluate.py
@@ -447,15 +447,15 @@ if __name__ == '__main__':
     DEBUG = False
     OUTPUT_DIR = os.path.expanduser('~/data/paper_stats/')
     ckpt_dirs = [
-        '/home/ashesh.ashesh/training/disentangle/2211/D3-M10-S0-L3/18',
+        '/home/ashesh.ashesh/training/disentangle/2211/D3-M10-S0-L3/8',
     ]
     ckpt_dirs = [x[:-1] if '/' == x[-1] else x for x in ckpt_dirs]
     mmse_count = 1
 
-    for custom_image_size in [64]:
+    for custom_image_size in [1024]:
         for eval_datasplit_type in [DataSplitType.Test]:
             for ckpt_dir in ckpt_dirs:
-                for image_size_for_grid_centers in [16]:
+                for image_size_for_grid_centers in [256]:
                     ignored_last_pixels = 32 if os.path.basename(
                         os.path.dirname(ckpt_dir)).split('-')[0][1:] == '3' else 0
                     handler = PaperResultsHandler(OUTPUT_DIR, eval_datasplit_type, custom_image_size,
