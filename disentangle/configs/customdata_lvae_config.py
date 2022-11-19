c8121446964327f686cc8a934c3bd905375da6fb
ashesh <ashesh276@gmail.com>
2022-11-19 13:01:59 +0000

    config update

--- disentangle/configs/customdata_lvae_config.py
+++ disentangle/configs/customdata_lvae_config.py
@@ -19,7 +19,7 @@ def get_config():
     data.curve_thickness = 21
     data.max_vshift_factor = 0.9
     data.max_hshift_factor = 0.3
-    data.frequency_range_list = [(0.05, 0.07), (0.12, 0.14), (0.3, 0.32), (0.6, 0.62)]
+    data.frequency_range_list = [(0.03, 0.07), (0.12, 0.20), (0.3, 0.45), (0.55, 0.7)]
 
     data.sampler_type = SamplerType.DefaultSampler
     data.deterministic_grid = False
