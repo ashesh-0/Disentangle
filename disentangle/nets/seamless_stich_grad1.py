dd727a2c0eba06a15470395b3e2aac80cec22aa5
ashesh <ashesh276@gmail.com>
2022-12-03 10:19:57 +0000

    fix in grad based stitch

--- disentangle/nets/seamless_stich_grad1.py
+++ disentangle/nets/seamless_stich_grad1.py
@@ -27,42 +27,46 @@ class SeamlessStitchGrad1(SeamlessStitch):
         if col_idx == 0:
             return 0.0
         p = self.params[row_idx, col_idx]
+        nbr_p = self.params[row_idx, col_idx - 1].item()
 
         left_p_gradient = self.get_lgradient(row_idx, col_idx)
         right_p_gradient = self.get_rgradient(row_idx, col_idx - 1)
         avg_gradient = (left_p_gradient + right_p_gradient) / 2
         boundary_gradient = self.get_lneighbor_gradient(row_idx, col_idx)
-        return self._compute_loss_on_boundaries(boundary_gradient, avg_gradient, p)
+        return self._compute_loss_on_boundaries(boundary_gradient, avg_gradient, p - nbr_p)
 
     def _compute_right_loss(self, row_idx, col_idx):
         if col_idx == self.params.shape[1] - 1:
             return 0.0
         p = self.params[row_idx, col_idx]
+        nbr_p = self.params[row_idx, col_idx + 1].item()
 
         left_p_gradient = self.get_lgradient(row_idx, col_idx + 1)
         right_p_gradient = self.get_rgradient(row_idx, col_idx)
         avg_gradient = (left_p_gradient + right_p_gradient) / 2
         boundary_gradient = self.get_rneighbor_gradient(row_idx, col_idx)
-        return self._compute_loss_on_boundaries(boundary_gradient, avg_gradient, -1 * p)
+        return self._compute_loss_on_boundaries(boundary_gradient, avg_gradient, nbr_p - p)
 
     def _compute_top_loss(self, row_idx, col_idx):
         if row_idx == 0:
             return 0.0
         p = self.params[row_idx, col_idx]
+        nbr_p = self.params[row_idx - 1, col_idx].item()
 
         top_p_gradient = self.get_tgradient(row_idx, col_idx)
         bottom_p_gradient = self.get_bgradient(row_idx - 1, col_idx)
         avg_gradient = (top_p_gradient + bottom_p_gradient) / 2
         boundary_gradient = self.get_tneighbor_gradient(row_idx, col_idx)
-        return self._compute_loss_on_boundaries(boundary_gradient, avg_gradient, p)
+        return self._compute_loss_on_boundaries(boundary_gradient, avg_gradient, p - nbr_p)
 
     def _compute_bottom_loss(self, row_idx, col_idx):
         if row_idx == self.params.shape[1] - 1:
             return 0.0
         p = self.params[row_idx, col_idx]
+        nbr_p = self.params[row_idx + 1, col_idx].item()
 
         top_p_gradient = self.get_tgradient(row_idx + 1, col_idx)
         bottom_p_gradient = self.get_bgradient(row_idx, col_idx)
         avg_gradient = (top_p_gradient + bottom_p_gradient) / 2
         boundary_gradient = self.get_bneighbor_gradient(row_idx, col_idx)
-        return self._compute_loss_on_boundaries(boundary_gradient, avg_gradient, p)
+        return self._compute_loss_on_boundaries(boundary_gradient, avg_gradient, nbr_p - p)
