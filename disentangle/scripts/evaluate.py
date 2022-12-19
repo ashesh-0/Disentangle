917de77ce2ad392c1540c5f5ba9b28ed0daaff08
ashesh <ashesh276@gmail.com>
2022-12-19 21:46:15 +0000

    eval

--- disentangle/scripts/evaluate.py
+++ disentangle/scripts/evaluate.py
@@ -445,7 +445,7 @@ def main(
 
 if __name__ == '__main__':
     DEBUG = False
-    ckpt_dirs = ['/home/ashesh.ashesh/training/disentangle/2212/D3-M11-S0-L3/2']
+    ckpt_dirs = ['/home/ashesh.ashesh/training/disentangle/2212/D3-M11-S0-L3/0']
     if ckpt_dirs[0].startswith('/home/ashesh.ashesh'):
         OUTPUT_DIR = os.path.expanduser('/group/jug/ashesh/data/paper_stats/')
     elif ckpt_dirs[0].startswith('/home/ubuntu/ashesh'):
@@ -456,7 +456,7 @@ if __name__ == '__main__':
     ckpt_dirs = [x[:-1] if '/' == x[-1] else x for x in ckpt_dirs]
     mmse_count = 1
 
-    for custom_image_size in [512]:
+    for custom_image_size in [256]:
         for eval_datasplit_type in [DataSplitType.Test]:
             for ckpt_dir in ckpt_dirs:
                 for image_size_for_grid_centers in [128]:
