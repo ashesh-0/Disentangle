6ed318fb473db33361fc56a75e0ab4b365b15787
ashesh <ashesh276@gmail.com>
2022-12-29 22:06:56 +0000

    config update

--- disentangle/configs/microscopy_multi_channel_lvae_config.py
+++ disentangle/configs/microscopy_multi_channel_lvae_config.py
@@ -46,7 +46,7 @@ def get_config():
 
     model = config.model
     model.model_type = ModelType.LadderVae
-    model.z_dims = [128, 128, 128, 128, 128, 128, 128, 128, 128, 128]
+    model.z_dims = [128, 128, 128, 128, 128, 128, 128, 128, 128]
 
     model.encoder.blocks_per_layer = 1
     model.encoder.n_filters = 64
