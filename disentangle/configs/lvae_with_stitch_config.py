eb52a5f4df85358a81ccd67eeea8f5d5669b3803
ashesh <ashesh276@gmail.com>
2022-12-31 12:44:23 +0000

    config update

--- disentangle/configs/lvae_with_stitch_config.py
+++ disentangle/configs/lvae_with_stitch_config.py
@@ -25,7 +25,7 @@ def get_config():
     data.use_one_mu_std = True
     data.train_aug_rotate = False
     data.randomized_channels = False
-    data.multiscale_lowres_count = None
+    data.multiscale_lowres_count = 5
     data.padding_mode = 'reflect'
     data.padding_value = None
     # If this is set to True, then target channels will be normalized from their separate mean.
@@ -78,7 +78,7 @@ def get_config():
     # predict_logvar takes one of the four values: [None,'global','channelwise','pixelwise']
     model.predict_logvar = 'pixelwise'
     model.logvar_lowerbound = -5  # -2.49 is log(1/12), from paper "Re-parametrizing VAE for stablity."
-    model.multiscale_lowres_separate_branch = 5
+    model.multiscale_lowres_separate_branch = False
     model.multiscale_retain_spatial_dims = True
     model.monitor = 'val_psnr'  # {'val_loss','val_psnr'}
     model.non_stochastic_version = False
