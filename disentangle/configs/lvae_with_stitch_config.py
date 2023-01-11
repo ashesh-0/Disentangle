3d26a86c900e9ff55a07a655014d2c9e26585d5b
ashesh <ashesh276@gmail.com>
2023-01-11 10:26:03 +0000

    config update

--- disentangle/configs/lvae_with_stitch_config.py
+++ disentangle/configs/lvae_with_stitch_config.py
@@ -87,7 +87,7 @@ def get_config():
     model.offset_latent_dims = 50
     model.offset_prediction_scalar_prediction = True
     model.regularize_offset = True
-    model.offset_regularization_w = 0.01
+    model.offset_regularization_w = 0.001
 
     training = config.training
     training.lr = 0.001
