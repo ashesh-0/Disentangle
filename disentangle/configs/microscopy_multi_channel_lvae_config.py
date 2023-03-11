6aa229d28cb346c62a43fb0f3e5e3c51e9f2aade
Ashesh <ashesh276@gmail.com>
2023-03-11 23:28:07 +0100

    config update

--- disentangle/configs/microscopy_multi_channel_lvae_config.py
+++ disentangle/configs/microscopy_multi_channel_lvae_config.py
@@ -28,7 +28,7 @@ def get_config():
     data.use_one_mu_std = True
     data.train_aug_rotate = False
     data.randomized_channels = False
-    data.multiscale_lowres_count = 5
+    data.multiscale_lowres_count = None
     data.padding_mode = 'reflect'
     data.padding_value = None
     # If this is set to True, then target channels will be normalized from their separate mean.
