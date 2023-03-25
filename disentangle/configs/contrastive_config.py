8a562afa5c19d5f39b250ec9d38e66ed0f57309f
ashesh <ashesh276@gmail.com>
2023-03-25 08:41:23 +0000

    config update

--- disentangle/configs/contrastive_config.py
+++ disentangle/configs/contrastive_config.py
@@ -79,7 +79,7 @@ def get_config():
     model.decoder.res_block_kernel = 3
     model.decoder.res_block_skip_padding = False
     model.decoder.multiscale_retain_spatial_dims = True
-    model.decoder.skip_bottom_k_bu_values = 2
+    # model.decoder.skip_bottom_k_bu_values = 2
     model.decoder.conv2d_bias = False
 
     model.skip_nboundary_pixels_from_loss = None
