00c372a39cd3f71c92af2808fffa81276d8c9282
Ashesh <ashesh276@gmail.com>
2023-03-27 08:02:17 +0200

    config update

--- disentangle/configs/contrastive_config.py
+++ disentangle/configs/contrastive_config.py
@@ -111,15 +111,15 @@ def get_config():
 
     training = config.training
     training.lr = 0.001
-    training.lr_scheduler_patience = 90
-    training.max_epochs = 1200
-    training.batch_size = 64
+    training.lr_scheduler_patience = 45
+    training.max_epochs = 600
+    training.batch_size = 32
     training.num_workers = 4
     training.val_repeat_factor = None
     training.train_repeat_factor = None
     training.val_fraction = 0.1
     training.test_fraction = 0.1
-    training.earlystop_patience = 600
+    training.earlystop_patience = 300
     training.precision = 16
 
     return config
