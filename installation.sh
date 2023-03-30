4a36c96e10626eb19feef58ef53b418880ed82c9
Ashesh <ashesh276@gmail.com>
2023-03-30 21:47:24 +0200

    installation update

--- installation.sh
+++ installation.sh
@@ -1,6 +1,6 @@
 conda create -n Disentangle python=3.9
 conda activate Disentangle
-conda install pytorch==1.13.1 torchvision==0.14.1 pytorch-cuda=11.6 -c pytorch -c nvidia
+conda install pytorch==1.13.1 torchvision==0.14.1 git=11.6 -c pytorch -c nvidia
 conda install -c conda-forge pytorch-lightning
 conda install -c conda-forge wandb -y
 conda install -c conda-forge tensorboard -y
@@ -14,3 +14,5 @@ conda install -c conda-forge nd2reader -y
 conda install -c conda-forge yapf -y
 conda install -c conda-forge isort -y
 python -m pip install pre-commit
+conda install -c conda-forge czifile -y
+
