f41f2e76f9e91acfd99c5036febe33ec044b2895
Ashesh <ashesh276@gmail.com>
2023-04-01 14:51:21 +0200

    config update

--- disentangle/configs/microscopy_multi_channel_lvae_config.py
+++ disentangle/configs/microscopy_multi_channel_lvae_config.py
@@ -16,7 +16,7 @@ def get_config():
     data.image_size = 64
     data.data_type = DataType.OptiMEM100_014
     data.channel_1 = 0
-    data.channel_2 = 3
+    data.channel_2 = 2
 
     data.sampler_type = SamplerType.DefaultSampler
     data.deterministic_grid = False
@@ -47,8 +47,8 @@ def get_config():
     loss.free_bits = 0.0
     loss.enable_receptive_field_priorloss = True
     loss.receptive_field_prior_losstype = ConvolutionPriorLossType.FactorBased
-    loss.receptive_field_prior_w = 400
-    loss.receptive_field_prior_loss_factor = 0.07
+    loss.receptive_field_prior_w = 100
+    loss.receptive_field_prior_loss_factor = 0.05
 
     # Old version needed these.
     # loss.receptive_field_prior_loss_minclip = -0.3
