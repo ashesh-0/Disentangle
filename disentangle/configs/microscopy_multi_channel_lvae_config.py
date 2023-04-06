143a2bcd910f90c5f4f7ecc67565bb5d5230b2e3
Ashesh <ashesh276@gmail.com>
2023-04-06 11:17:24 +0200

    config update

--- disentangle/configs/microscopy_multi_channel_lvae_config.py
+++ disentangle/configs/microscopy_multi_channel_lvae_config.py
@@ -48,7 +48,7 @@ def get_config():
     loss.free_bits = 0.0
     loss.enable_receptive_field_priorloss = True
     loss.receptive_field_prior_losstype = ConvolutionPriorLossType.FactorBased
-    loss.receptive_field_prior_w = 200
+    loss.receptive_field_prior_w = 100
     # loss.receptive_field_prior_loss_minclip = -0.3
     loss.receptive_field_prior_loss_factor = 0.03
     loss.skip_receptive_field_loss_tokens = ['final_top_down', 'likelihood', 'top_prior_params']
