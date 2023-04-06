c67ca4d439a1add3de1730981a23a91c55070f12
Ashesh <ashesh276@gmail.com>
2023-04-06 23:00:01 +0200

    config update

--- disentangle/configs/pavia2Vanilla_config.py
+++ disentangle/configs/pavia2Vanilla_config.py
@@ -49,7 +49,7 @@ def get_config():
     loss.kl_start = -1
     loss.kl_min = 1e-7
     loss.free_bits = 0.0
-    loss.lres_recloss_w = [0.52, 0.16, 0.16, 0.16]
+    # loss.lres_recloss_w = [0.52, 0.16, 0.16, 0.16]
 
     model = config.model
     model.model_type = ModelType.LadderVae
