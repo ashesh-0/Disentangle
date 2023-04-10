4e5989910acf8580f2c174022eed3b1717e8f182
Ashesh <ashesh276@gmail.com>
2023-04-10 18:27:46 +0000

    config update

--- disentangle/configs/ht_iba1_ki64_config.py
+++ disentangle/configs/ht_iba1_ki64_config.py
@@ -26,7 +26,7 @@ def get_config():
     data.use_one_mu_std = True
     data.train_aug_rotate = False
     data.randomized_channels = False
-    data.multiscale_lowres_count = None
+    data.multiscale_lowres_count = 5
     data.padding_mode = 'reflect'
     data.padding_value = None
     # If this is set to True, then target channels will be normalized from their separate mean.
@@ -34,7 +34,7 @@ def get_config():
     data.target_separate_normalization = True
 
     # Replacing one channel's content with empty patch.
-    data.empty_patch_replacement_enabled = True
+    data.empty_patch_replacement_enabled = False
     data.empty_patch_replacement_channel_idx = 0
     data.empty_patch_replacement_probab = 0.3
     data.empty_patch_max_val_threshold = 130
@@ -68,7 +68,7 @@ def get_config():
     model.decoder.res_block_kernel = 3
     model.decoder.res_block_skip_padding = False
 
-    model.decoder.multiscale_retain_spatial_dims = False
+    model.decoder.multiscale_retain_spatial_dims = True
 
     model.skip_nboundary_pixels_from_loss = None
     model.nonlin = 'elu'
