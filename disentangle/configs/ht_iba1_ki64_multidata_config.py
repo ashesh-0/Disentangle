6e7b277c2217e3cf84b1df65d3263e91fdcb217e
Ashesh <ashesh276@gmail.com>
2023-05-16 08:58:53 +0200

    config update

--- disentangle/configs/ht_iba1_ki64_multidata_config.py
+++ disentangle/configs/ht_iba1_ki64_multidata_config.py
@@ -48,7 +48,7 @@ def get_config():
     # Replacing one channel's content with empty patch.
     data.empty_patch_replacement_enabled = False
     data.empty_patch_replacement_channel_idx = 0
-    data.empty_patch_replacement_probab = 0.5
+    data.empty_patch_replacement_probab = 0.8
     data.empty_patch_max_val_threshold = 180
 
     loss = config.loss
