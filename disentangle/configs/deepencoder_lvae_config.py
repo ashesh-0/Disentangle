5d5f91d4b24fbfa39bee27e2896edc095268c337
Ashesh <ashesh276@gmail.com>
2023-06-21 16:09:08 +0200

    with stochasticity enabled

--- disentangle/configs/deepencoder_lvae_config.py
+++ disentangle/configs/deepencoder_lvae_config.py
@@ -111,7 +111,7 @@ def get_config():
     model.multiscale_lowres_separate_branch = False
     model.multiscale_retain_spatial_dims = True
     model.monitor = 'val_psnr'  # {'val_loss','val_psnr'}
-    model.non_stochastic_version = True
+    model.non_stochastic_version = False
 
     training = config.training
     training.lr = 0.001
