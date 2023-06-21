04f9e66df864ba9b4eb4723c6cc55e0966ae7890
Ashesh <ashesh276@gmail.com>
2023-06-21 13:39:55 +0200

    config update

--- disentangle/configs/deepencoder_lvae_config.py
+++ disentangle/configs/deepencoder_lvae_config.py
@@ -17,13 +17,13 @@ def get_config():
     data.channel_1 = 2
     data.channel_2 = 3
 
-    data.ch1_min_alpha = 0.2
-    data.ch1_max_alpha = 0.6
+    data.ch1_min_alpha = 0.3
+    data.ch1_max_alpha = 0.5
     data.ch1_alpha_interval_count = 20
     data.num_intensity_variations = 2
     data.return_individual_channels = True
 
-    data.sampler_type = SamplerType.DefaultSampler
+    data.sampler_type = SamplerType.IntensityAugSampler
 
     data.deterministic_grid = False
     data.normalized_input = True
