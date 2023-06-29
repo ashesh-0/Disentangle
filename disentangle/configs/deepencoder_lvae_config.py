868974f5879df9c2156b2073eb6dd69f4c14825c
Ashesh <ashesh276@gmail.com>
2023-06-29 15:21:58 +0200

    config update

--- disentangle/configs/deepencoder_lvae_config.py
+++ disentangle/configs/deepencoder_lvae_config.py
@@ -46,7 +46,7 @@ def get_config():
     data.use_one_mu_std = True
     data.train_aug_rotate = False
     data.randomized_channels = False
-    data.multiscale_lowres_count = None
+    data.multiscale_lowres_count = 5
     data.padding_mode = 'reflect'
     data.padding_value = None
     # If this is set to True, then target channels will be normalized from their separate mean.
