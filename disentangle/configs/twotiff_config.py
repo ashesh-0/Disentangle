e5936c5b1b01ae1d8092adbe7632cb21566c64a7
Ashesh <ashesh276@gmail.com>
2023-07-09 12:39:29 +0200

    config update

--- disentangle/configs/twotiff_config.py
+++ disentangle/configs/twotiff_config.py
@@ -87,7 +87,7 @@ def get_config():
 
     model.enable_noise_model = True
     model.noise_model_type = 'gmm'
-    fname_format = '/home/ashesh.ashesh/training/noise_model/{}/GMMNoiseModel_ventura_gigascience-{}_12_4_Clip0.5-100_Sig0.125_UpNone_Norm1_bootstrap.npz'
+    fname_format = '/home/ashesh.ashesh/training/noise_model/{}/GMMNoiseModel_ventura_gigascience-{}_24_4_Clip0.5-100_Sig0.125_UpNone_Norm1_bootstrap.npz'
     model.noise_model_ch1_fpath = fname_format.format('2307/20', 'actin')
     model.noise_model_ch2_fpath = fname_format.format('2307/19', 'mito')
     model.non_stochastic_version = False
