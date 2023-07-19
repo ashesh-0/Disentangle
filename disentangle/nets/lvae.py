cfd5492b7f3c8fffa4c4113b6bdafaee7f27e7e9
Ashesh <ashesh276@gmail.com>
2023-07-19 14:03:21 +0200

    kl loss like before

--- disentangle/nets/lvae.py
+++ disentangle/nets/lvae.py
@@ -507,12 +507,16 @@ class LadderVAE(pl.LightningModule):
     def get_kl_divergence_loss(self, topdown_layer_data_dict):
         # kl[i] for each i has length batch_size
         # resulting kl shape: (batch_size, layers)
-        kl = torch.cat([kl_layer.unsqueeze(1) for kl_layer in topdown_layer_data_dict['kl']], dim=1)
-        nlayers = kl.shape[1]
-        for i in range(nlayers):
-            kl[:, i] = kl[:, i] / np.prod(topdown_layer_data_dict['z'][i].shape[-3:])
+        # kl = torch.cat([kl_layer.unsqueeze(1) for kl_layer in topdown_layer_data_dict['kl']], dim=1)
+        # nlayers = kl.shape[1]
+        # for i in range(nlayers):
+        #     kl[:, i] = kl[:, i] / np.prod(topdown_layer_data_dict['z'][i].shape[-3:])
 
-        kl_loss = free_bits_kl(kl, self.free_bits).mean()
+        # kl_loss = free_bits_kl(kl, self.free_bits).mean()
+        kl = torch.cat([kl_layer.unsqueeze(1) for kl_layer in topdown_layer_data_dict['kl']], dim=1)
+
+        kl_loss = free_bits_kl(kl, self.free_bits).sum()  # sum over layers
+
         return kl_loss
 
     #   NOTE: Gradient logging has been removed because of a version issue. The issue is that
