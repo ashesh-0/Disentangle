de0ba28e36d40124a4d22836c6b9603d37dec180
Ashesh <ashesh276@gmail.com>
2023-07-24 13:07:33 +0200

    config update

--- disentangle/configs/twotiff_config.py
+++ disentangle/configs/twotiff_config.py
@@ -38,7 +38,7 @@ def get_config():
     loss.loss_type = LossType.Elbo
     # loss.mixed_rec_weight = 1
 
-    loss.kl_weight = 0.25
+    loss.kl_weight = 2
     loss.kl_annealing = False
     loss.kl_annealtime = 10
     loss.kl_start = -1
