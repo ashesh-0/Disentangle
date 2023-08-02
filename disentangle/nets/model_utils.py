fe557c783f21c9a7d481328301aa8caae96f7ab5
Ashesh <ashesh276@gmail.com>
2023-08-02 09:59:54 +0200

    cleaned up everything not being used

--- disentangle/nets/model_utils.py
+++ disentangle/nets/model_utils.py
@@ -11,56 +11,16 @@ from disentangle.core.loss_type import LossType
 from disentangle.core.model_type import ModelType
 from disentangle.nets.brave_net import BraveNetPL
 from disentangle.nets.lvae import LadderVAE
-from disentangle.nets.lvae_bleedthrough import LadderVAEWithMixedRecons
-from disentangle.nets.lvae_deepencoder import LVAEWithDeepEncoder
-from disentangle.nets.lvae_multidset import LadderVaeMultiDataset
-from disentangle.nets.lvae_multidset_multi_input_branches import LadderVaeMultiDatasetMultiBranch
-from disentangle.nets.lvae_multidset_multi_optim import LadderVaeMultiDatasetMultiOptim
-from disentangle.nets.lvae_multiple_encoder_single_opt import LadderVAEMulEncoder1Optim
-from disentangle.nets.lvae_multiple_encoders import LadderVAEMultipleEncoders
-from disentangle.nets.lvae_multires_target import LadderVAEMultiTarget
-from disentangle.nets.lvae_semi_supervised import LadderVAESemiSupervised
-from disentangle.nets.lvae_twindecoder import LadderVAETwinDecoder
-from disentangle.nets.lvae_with_critic import LadderVAECritic
-from disentangle.nets.lvae_with_stitch import LadderVAEwithStitching
-from disentangle.nets.lvae_with_stitch_2stage import LadderVAEwithStitching2Stage
 from disentangle.nets.unet import UNet
 
 
 def create_model(config, data_mean, data_std):
     if config.model.model_type == ModelType.LadderVae:
         model = LadderVAE(data_mean, data_std, config)
-    elif config.model.model_type == ModelType.LadderVaeTwinDecoder:
-        model = LadderVAETwinDecoder(data_mean, data_std, config)
-    elif config.model.model_type == ModelType.LadderVAECritic:
-        model = LadderVAECritic(data_mean, data_std, config)
-    elif config.model.model_type == ModelType.LadderVaeSepEncoder:
-        model = LadderVAEMultipleEncoders(data_mean, data_std, config)
-    elif config.model.model_type == ModelType.LadderVAEMultiTarget:
-        model = LadderVAEMultiTarget(data_mean, data_std, config)
-    elif config.model.model_type == ModelType.LadderVaeSepEncoderSingleOptim:
-        model = LadderVAEMulEncoder1Optim(data_mean, data_std, config)
     elif config.model.model_type == ModelType.UNet:
         model = UNet(data_mean, data_std, config)
     elif config.model.model_type == ModelType.BraveNet:
         model = BraveNetPL(data_mean, data_std, config)
-    elif config.model.model_type == ModelType.LadderVaeStitch:
-        model = LadderVAEwithStitching(data_mean, data_std, config)
-    elif config.model.model_type == ModelType.LadderVaeMixedRecons:
-        model = LadderVAEWithMixedRecons(data_mean, data_std, config)
-    elif config.model.model_type == ModelType.LadderVaeSemiSupervised:
-        model = LadderVAESemiSupervised(data_mean, data_std, config)
-    elif config.model.model_type == ModelType.LadderVaeStitch2Stage:
-        model = LadderVAEwithStitching2Stage(data_mean, data_std, config)
-    elif config.model.model_type == ModelType.LadderVaeMultiDataSet:
-        model = LadderVaeMultiDataset(data_mean, data_std, config)
-    elif config.model.model_type == ModelType.LadderVaeMultiDatasetMultiBranch:
-        model = LadderVaeMultiDatasetMultiBranch(data_mean, data_std, config)
-    elif config.model.model_type == ModelType.LadderVaeMultiDatasetMultiOptim:
-        model = LadderVaeMultiDatasetMultiOptim(data_mean, data_std, config)
-    elif config.model.model_type == ModelType.LVaeDeepEncoderIntensityAug:
-        model = LVAEWithDeepEncoder(data_mean, data_std, config)
-
     else:
         raise Exception('Invalid model type:', config.model.model_type)
     return model
