262647170652ed3723f2ba93a5d7e56b5d553aeb
Ashesh <ashesh276@gmail.com>
2023-08-07 22:26:33 +0200

    fix

--- usplit/losses.py
+++ usplit/losses.py
@@ -12,8 +12,6 @@ import torch.optim as optim
 from torch.autograd import Variable
 from torch.nn import init
 
-from disentangle import utils
-
 
 def free_bits_kl(kl, free_bits, batch_average=False, eps=1e-6) -> torch.Tensor:
     """Computes free-bits version of KL divergence.
