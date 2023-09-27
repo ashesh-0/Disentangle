c9705dee72296efdfd5f24b6b589c466d9215fd9
Ashesh <ashesh276@gmail.com>
2023-09-27 22:32:24 +0200

    config update

--- disentangle/configs/autoregressive_config.py
+++ disentangle/configs/autoregressive_config.py
@@ -79,7 +79,7 @@ def get_config():
 
     model = config.model
     model.model_type = ModelType.AutoRegresiveRALadderVAE
-    model.rotation_with_neighbors = False
+    model.rotation_with_neighbors = True
     model.untrained_nbr_branch = False
     model.z_dims = [128, 128, 128, 128]
     model.nbr_dropout = 0.2
