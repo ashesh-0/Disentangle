72369c4fba9f9e4c643c9604eb6ad08ca40f79f2
Ashesh <ashesh276@gmail.com>
2023-10-04 08:45:12 +0200

    config update

--- disentangle/configs/autoregressive_config.py
+++ disentangle/configs/autoregressive_config.py
@@ -13,8 +13,8 @@ from disentangle.data_loader.patch_index_manager import GridAlignement
 def get_config():
     config = get_default_config()
     data = config.data
-    data.image_size = 128
-    data.val_grid_size = 96
+    data.image_size = 64
+    data.val_grid_size = 32
     data.grid_alignment = GridAlignement.Center
     data.full_coverage_randomized = True
     data.data_type = DataType.OptiMEM100_014
@@ -52,7 +52,7 @@ def get_config():
     data.use_one_mu_std = True
     data.train_aug_rotate = False
     data.randomized_channels = False
-    data.multiscale_lowres_count = None
+    data.multiscale_lowres_count = 5
     data.padding_mode = 'reflect'
     data.padding_value = None
     # If this is set to True, then target channels will be normalized from their separate mean.
