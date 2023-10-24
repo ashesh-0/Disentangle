d67108e28111aa8ef8159af0dee02da68b672d20
Ashesh <ashesh276@gmail.com>
2023-10-24 23:10:35 +0200

    config update

--- disentangle/configs/autoregressive_config.py
+++ disentangle/configs/autoregressive_config.py
@@ -85,7 +85,7 @@ def get_config():
     model.z_dims = [128, 128, 128, 128]
     model.nbr_dropout = 0.2
     model.nbr_share_weights = True
-    model.nbr_disabled = True
+    model.nbr_disabled = False
     model.enable_after_nepoch = 5
     # model.skip_nbr_in_bottomk_levels = 1
 
