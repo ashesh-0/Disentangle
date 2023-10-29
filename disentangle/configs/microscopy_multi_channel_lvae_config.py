5b8b627ae2c4c37960a7165eca81a1be05f094fa
Ashesh <ashesh276@gmail.com>
2023-10-29 16:15:43 +0000

    config update

--- disentangle/configs/microscopy_multi_channel_lvae_config.py
+++ disentangle/configs/microscopy_multi_channel_lvae_config.py
@@ -117,8 +117,8 @@ def get_config():
 
     training = config.training
     training.lr = 0.001 / 2
-    training.lr_scheduler_patience = 990
-    training.max_epochs = 6600
+    training.lr_scheduler_patience = 30
+    training.max_epochs = 200
     training.batch_size = 32
     training.num_workers = 4
     training.val_repeat_factor = None
@@ -126,8 +126,8 @@ def get_config():
     training.val_fraction = 0.1
     training.test_fraction = 0.1
 
-    training.earlystop_patience = 3300
+    training.earlystop_patience = 100
     training.precision = 16
-    training.check_val_every_n_epoch = 10
+    # training.check_val_every_n_epoch = 10
 
     return config
