b46b7ddac6f4dfd26607d8c2259e75e5d419c8d6
Ashesh <ashesh276@gmail.com>
2023-10-30 08:12:49 +0000

    config update

--- disentangle/configs/twotiff_config.py
+++ disentangle/configs/twotiff_config.py
@@ -16,9 +16,9 @@ def get_config():
     data.ch2_fname = 'mito-60x-noise2-highsnr.tif'
 
     # from the training data, we will use only this fraction of data. This is useful for working with less supervised data.
-    # data.trainig_datausage_fraction = 0.1
-    data.training_validtarget_fraction = 0.02
-    data.validtarget_random_fraction = 0.7
+    data.trainig_datausage_fraction = 0.02
+    # data.training_validtarget_fraction = 0.02
+    # data.validtarget_random_fraction = 0.7
     # data.validtarget_random_fraction_final = 0.9
     # data.validtarget_random_fraction_stepepoch = 0.005
     data.sampler_type = SamplerType.DefaultSampler
@@ -117,8 +117,8 @@ def get_config():
 
     training = config.training
     training.lr = 0.001 / 2
-    training.lr_scheduler_patience = 30
-    training.max_epochs = 200
+    training.lr_scheduler_patience = 1500
+    training.max_epochs = 10000
     training.batch_size = 32
     training.num_workers = 0
     training.val_repeat_factor = None
@@ -126,7 +126,7 @@ def get_config():
     training.val_fraction = 0.1
     training.test_fraction = 0.1
 
-    training.earlystop_patience = 100
+    training.earlystop_patience = 5000
     training.precision = 16
 
     return config
