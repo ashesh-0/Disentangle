0e2ac52e5d6233294d8336b0a3f667db4799656f
Ashesh <ashesh276@gmail.com>
2023-10-30 08:10:30 +0000

    config update

--- disentangle/configs/twotiff_config.py
+++ disentangle/configs/twotiff_config.py
@@ -12,15 +12,15 @@ def get_config():
     data.data_type = DataType.SeparateTiffData
     data.channel_1 = 0
     data.channel_2 = 1
-    data.ch1_fname = 'actin-60x-noise2-lowsnr.tif'
-    data.ch2_fname = 'mito-60x-noise2-lowsnr.tif'
+    data.ch1_fname = 'actin-60x-noise2-highsnr.tif'
+    data.ch2_fname = 'mito-60x-noise2-highsnr.tif'
 
     # from the training data, we will use only this fraction of data. This is useful for working with less supervised data.
     # data.trainig_datausage_fraction = 0.1
-    data.training_validtarget_fraction = 0.03
-    data.validtarget_random_fraction = 0.3
-    data.validtarget_random_fraction_final = 0.9
-    data.validtarget_random_fraction_stepepoch = 0.005
+    data.training_validtarget_fraction = 0.02
+    data.validtarget_random_fraction = 0.7
+    # data.validtarget_random_fraction_final = 0.9
+    # data.validtarget_random_fraction_stepepoch = 0.005
     data.sampler_type = SamplerType.DefaultSampler
     data.deterministic_grid = False
     data.normalized_input = True
--- disentangle/data_loader/multi_channel_determ_tiff_dloader.py
+++ disentangle/data_loader/multi_channel_determ_tiff_dloader.py
@@ -137,7 +137,6 @@ class MultiChDeterministicTiffDloader:
                                         val_fraction=val_fraction,
                                         test_fraction=test_fraction,
                                         allow_generation=allow_generation)
-        import pdb;pdb.set_trace()
         if self._datausage_fraction < 1.0:
             cnt = int(self._data.shape[0] * self._datausage_fraction)
             print(f'[{self.__class__.__name__}] Using only {cnt} out of {self._data.shape[0]} training data.')
