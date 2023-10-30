38cfd0007e22dcba0f0af5a0c4a64b7e844f2563
ashesh <ashesh276@gmail.com>
2023-10-30 08:05:06 +0000

    updated for twotiff data

--- disentangle/configs/twotiff_config.py
+++ disentangle/configs/twotiff_config.py
@@ -14,15 +14,30 @@ def get_config():
     data.channel_2 = 1
     data.ch1_fname = 'actin-60x-noise2-lowsnr.tif'
     data.ch2_fname = 'mito-60x-noise2-lowsnr.tif'
-    data.enable_poisson_noise = True
 
+    # from the training data, we will use only this fraction of data. This is useful for working with less supervised data.
+    # data.trainig_datausage_fraction = 0.1
+    data.training_validtarget_fraction = 0.03
+    data.validtarget_random_fraction = 0.3
+    data.validtarget_random_fraction_final = 0.9
+    data.validtarget_random_fraction_stepepoch = 0.005
     data.sampler_type = SamplerType.DefaultSampler
-    data.threshold = 0.02
     data.deterministic_grid = False
     data.normalized_input = True
     data.clip_percentile = 0.995
+    data.background_quantile = 0.0
+    # With background quantile, one is setting the avg background value to 0. With this, any negative values are also set to 0.
+    # This, together with correct background_quantile should altogether get rid of the background. The issue here is that
+    # the background noise is also a distribution. So, some amount of background noise will remain.
+    data.clip_background_noise_to_zero = False
 
-    data.channelwise_quantile = True
+    # we will not subtract the mean of the dataset from every patch. We just want to subtract the background and normalize using std. This way, background will be very close to 0.
+    # this will help in the all scaling related approaches where we want to multiply the frame with some factor and then add them. we will then effectively just do these scaling on the
+    # foreground pixels and the background will anyways will remain very close to 0.
+    data.skip_normalization_using_mean = False
+
+    data.input_is_sum = False
+
     # If this is set to true, then one mean and stdev is used for both channels. Otherwise, two different
     # meean and stdev are used.
     data.use_one_mu_std = True
@@ -33,18 +48,25 @@ def get_config():
     data.padding_value = None
     # If this is set to True, then target channels will be normalized from their separate mean.
     # otherwise, target will be normalized just the same way as the input, which is determined by use_one_mu_std
-    data.target_separate_normalization = True
+    data.target_separate_normalization = False
+
+    # This is for intensity augmentation
+    # data.ch1_min_alpha = 0.4
+    # data.ch1_max_alpha = 0.55
+    # data.return_alpha = True
 
     loss = config.loss
-    loss.loss_type = LossType.Elbo
-    # loss.mixed_rec_weight = 1
+    loss.loss_type = LossType.ElboMixedReconstruction
+    loss.mixed_rec_weight = 1
 
     loss.kl_weight = 1
     loss.kl_annealing = False
     loss.kl_annealtime = 10
     loss.kl_start = -1
     loss.kl_min = 1e-7
-    loss.free_bits = 1.0
+    loss.free_bits = 0.0
+    # loss.ch1_recons_w = 1
+    # loss.ch2_recons_w = 5
 
     model = config.model
     model.model_type = ModelType.LadderVae
@@ -65,7 +87,8 @@ def get_config():
     model.decoder.res_block_skip_padding = False
 
     model.decoder.multiscale_retain_spatial_dims = False
-    config.model.decoder.conv2d_bias = True
+    model.decoder.conv2d_bias = True
+    model.reconstruction_mode = False
 
     model.skip_nboundary_pixels_from_loss = None
     model.nonlin = 'elu'
@@ -80,30 +103,29 @@ def get_config():
     model.analytical_kl = False
     model.mode_pred = False
     model.var_clip_max = 20
-    # predict_logvar takes one of the four values: [None,'global','channelwise','pixelwise']
-    model.predict_logvar = 'pixelwise'
+    # predict_logvar takes one of the four values: [None,'global','channelwise','pixelwise', 'ch_invariant_pixelwise]
+    model.predict_logvar = None
     model.logvar_lowerbound = -5  # -2.49 is log(1/12), from paper "Re-parametrizing VAE for stablity."
     model.multiscale_lowres_separate_branch = False
     model.multiscale_retain_spatial_dims = True
     model.monitor = 'val_psnr'  # {'val_loss','val_psnr'}
-
-    model.enable_noise_model = False
-    model.noise_model_type = 'gmm'
-    fname_format = '/home/ashesh.ashesh/training/noise_model/{}/GMMNoiseModel_ventura_gigascience-{}_6_4_Clip0.0-0.995_Sig0.125_UpNone_Norm1_bootstrap.npz'
-    model.noise_model_ch1_fpath = fname_format.format('2307/58', 'actin')
-    model.noise_model_ch2_fpath = fname_format.format('2307/59', 'mito')
     model.non_stochastic_version = False
+    model.enable_noise_model = False
+    model.noise_model_ch1_fpath = None
+    model.noise_model_ch1_fpath = None
+    # model.pretrained_weights_path = '/home/ubuntu/ashesh/training/disentangle/2310/D3-M3-S0-L0/2/kepler/BaselineVAECL_best.ckpt'
 
     training = config.training
-    training.lr = 0.001
-    training.lr_scheduler_patience = 15
+    training.lr = 0.001 / 2
+    training.lr_scheduler_patience = 30
     training.max_epochs = 200
-    training.batch_size = 16
-    training.num_workers = 4
+    training.batch_size = 32
+    training.num_workers = 0
     training.val_repeat_factor = None
     training.train_repeat_factor = None
     training.val_fraction = 0.1
     training.test_fraction = 0.1
+
     training.earlystop_patience = 100
     training.precision = 16
 
