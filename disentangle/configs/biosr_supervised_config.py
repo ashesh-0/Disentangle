ad605957ca1abbdca43b30ba3bfac2c6e65643b8
Ashesh <ashesh276@gmail.com>
2023-11-02 16:23:06 +0100

    config update

--- disentangle/configs/biosr_supervised_config.py
+++ disentangle/configs/biosr_supervised_config.py
@@ -112,7 +112,7 @@ def get_config():
     model.enable_noise_model = False
     model.noise_model_ch1_fpath = None
     model.noise_model_ch1_fpath = None
-    model.pretrained_weights_path = '/home/ashesh.ashesh/training/disentangle/2311/D16-M3-S0-L0/23/BaselineVAECL_best.ckpt'
+    # model.pretrained_weights_path = '/home/ashesh.ashesh/training/disentangle/2311/D16-M3-S0-L0/23/BaselineVAECL_best.ckpt'
 
     training = config.training
     training.lr = 0.001 / 2
