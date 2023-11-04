1e6cf911e3be5222659ef0f706072783b965e195
Ashesh <ashesh276@gmail.com>
2023-11-04 20:50:40 +0000

    config update

--- disentangle/configs/biosr_sparsely_supervised_config.py
+++ disentangle/configs/biosr_sparsely_supervised_config.py
@@ -75,7 +75,7 @@ def get_config():
     model = config.model
     model.model_type = ModelType.LadderVAEClassifierSupport
     model.classifier_fpath = '/home/ubuntu/ashesh/training/disentangle/texture_classifier.pth'
-    model.classifier_loss_weight = 0.003
+    model.classifier_loss_weight = 0.01
 
     model.z_dims = [128, 128, 128, 128]
 
