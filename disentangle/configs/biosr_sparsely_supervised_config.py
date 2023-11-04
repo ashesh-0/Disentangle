92bbfa6cdf53376e0ebc112d078f083402a0e4dd
Ashesh <ashesh276@gmail.com>
2023-11-04 22:10:23 +0100

    config update

--- disentangle/configs/biosr_sparsely_supervised_config.py
+++ disentangle/configs/biosr_sparsely_supervised_config.py
@@ -26,7 +26,7 @@ def get_config():
     data.validation_datausage_fraction = 0.08 * 2
 
     data.validtarget_random_fraction_final = 1
-    data.validtarget_random_fraction_stepepoch = 0.005
+    data.validtarget_random_fraction_stepepoch = 0.003
 
     data.sampler_type = SamplerType.DefaultSampler
     data.deterministic_grid = True
@@ -61,7 +61,7 @@ def get_config():
     loss = config.loss
     loss.loss_type = LossType.ElboMixedReconstruction
     loss.mixed_rec_weight = 1.0
-    loss.mixed_rec_w_step = 0.01
+    loss.mixed_rec_w_step = 0.007
 
     loss.kl_weight = 1
     loss.kl_annealing = False
