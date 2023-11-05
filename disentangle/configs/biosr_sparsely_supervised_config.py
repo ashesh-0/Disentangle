700baeb8579dc14e69e761679f68f666cb6fbabe
ashesh <ashesh276@gmail.com>
2023-11-05 19:45:27 +0000

    config update

--- disentangle/configs/biosr_sparsely_supervised_config.py
+++ disentangle/configs/biosr_sparsely_supervised_config.py
@@ -61,7 +61,7 @@ def get_config():
     loss = config.loss
     loss.loss_type = LossType.ElboMixedReconstruction
     loss.critic_loss_weight = 0.0005
-    loss.mixed_rec_weight = 1.0
+    loss.mixed_rec_weight = 0.3
     # loss.mixed_rec_w_step = 0.01
 
     loss.kl_weight = 1
