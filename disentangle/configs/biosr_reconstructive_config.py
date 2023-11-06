ae0d85e481b0bd41c0889912f8c5f8f1d0b9f3a9
Ashesh <ashesh276@gmail.com>
2023-11-06 20:07:45 +0100

    config ready

--- disentangle/configs/biosr_reconstructive_config.py
+++ disentangle/configs/biosr_reconstructive_config.py
@@ -104,12 +104,12 @@ def get_config():
     model.mode_pred = False
     model.var_clip_max = 20
     # predict_logvar takes one of the four values: [None,'global','channelwise','pixelwise', 'ch_invariant_pixelwise]
-    model.predict_logvar = None  #'ch_invariant_pixelwise'
+    model.predict_logvar = 'ch_invariant_pixelwise'
     model.logvar_lowerbound = -5  # -2.49 is log(1/12), from paper "Re-parametrizing VAE for stablity."
     model.multiscale_lowres_separate_branch = False
     model.multiscale_retain_spatial_dims = True
     model.monitor = 'val_psnr'  # {'val_loss','val_psnr'}
-    model.non_stochastic_version = True
+    model.non_stochastic_version = False
     model.enable_noise_model = False
     model.noise_model_ch1_fpath = None
     model.noise_model_ch1_fpath = None
@@ -117,9 +117,9 @@ def get_config():
 
     training = config.training
     training.lr = 0.001 / 2
-    training.lr_scheduler_patience = int(30 / data.trainig_datausage_fraction if 'trainig_datausage_fraction' in
-                                         data else 30)
-    training.max_epochs = int(200 / data.trainig_datausage_fraction if 'trainig_datausage_fraction' in data else 200)
+    training.lr_scheduler_patience = int(60 / data.trainig_datausage_fraction if 'trainig_datausage_fraction' in
+                                         data else 60)
+    training.max_epochs = int(400 / data.trainig_datausage_fraction if 'trainig_datausage_fraction' in data else 400)
     training.batch_size = 32
     training.num_workers = 2
     training.val_repeat_factor = None
@@ -127,8 +127,8 @@ def get_config():
     training.val_fraction = 0.1
     training.test_fraction = 0.1
 
-    training.earlystop_patience = int(100 /
-                                      data.trainig_datausage_fraction if 'trainig_datausage_fraction' in data else 100)
+    training.earlystop_patience = int(200 /
+                                      data.trainig_datausage_fraction if 'trainig_datausage_fraction' in data else 200)
     training.precision = 16
     training.check_val_every_n_epoch = int(
         1 / (data.trainig_datausage_fraction)) if 'trainig_datausage_fraction' in data else None
