8c1c756fe146d2c63759dd6f931f7cdd20ce299d
ashesh <ashesh276@gmail.com>
2023-11-13 14:53:12 +0000

    __init__

--- disentangle/nets/lvae_denoiser.py
+++ disentangle/nets/lvae_denoiser.py
@@ -5,6 +5,10 @@ from disentangle.nets.lvae import LadderVAE
 
 class LadderVAEDenoiser(LadderVAE):
 
+    def __init__(self, data_mean, data_std, config, use_uncond_mode_at=[], target_ch=2):
+        # since input is the target, we don't need to normalize it at all.
+        super().__init__(0, 1, config, use_uncond_mode_at=use_uncond_mode_at, target_ch=2)
+
     def training_step(self, batch, batch_idx, enable_logging=True):
         x, target = batch[:2]
         new_target = torch.tile(x[:, :1], (1, 2, 1, 1))
