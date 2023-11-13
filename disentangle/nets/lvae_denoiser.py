5a3529e983148de87c263a22a38c4d2f90585ab4
ashesh <ashesh276@gmail.com>
2023-11-13 14:19:18 +0000

    fixes

--- disentangle/nets/lvae_denoiser.py
+++ disentangle/nets/lvae_denoiser.py
@@ -7,14 +7,14 @@ class LadderVAEDenoiser(LadderVAE):
 
     def training_step(self, batch, batch_idx, enable_logging=True):
         x, target = batch[:2]
-        new_target = torch.tile(x, (1, 2, 1, 1))
+        new_target = torch.tile(x[:, :1], (1, 2, 1, 1))
         assert new_target.shape == target.shape
-        batch = (x, new_target) + batch[2:]
+        batch = (x, new_target, *batch[2:])
         return super().training_step(batch, batch_idx, enable_logging)
 
     def validation_step(self, batch, batch_idx):
         x, target = batch[:2]
-        new_target = torch.tile(x, (1, 2, 1, 1))
+        new_target = torch.tile(x[:, :1], (1, 2, 1, 1))
         assert new_target.shape == target.shape
-        batch = (x, new_target) + batch[2:]
+        batch = (x, new_target, *batch[2:])
         return super().validation_step(batch, batch_idx)
