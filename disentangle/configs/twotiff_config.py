1824f8e963af3ef1b7189d1fc0f456f46e4cea19
Ashesh <ashesh276@gmail.com>
2023-11-17 13:55:41 +0100

    config update

--- disentangle/configs/twotiff_config.py
+++ disentangle/configs/twotiff_config.py
@@ -101,7 +101,7 @@ def get_config():
     training.lr = 0.001
     training.lr_scheduler_patience = 15
     training.max_epochs = 200
-    training.batch_size = 16
+    training.batch_size = 32
     training.num_workers = 4
     training.val_repeat_factor = None
     training.train_repeat_factor = None
