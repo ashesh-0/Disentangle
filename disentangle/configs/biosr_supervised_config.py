e30ccafdbf9e238d21f8f67aef3f2f91af4af171
Ashesh <ashesh276@gmail.com>
2023-11-28 11:11:06 +0100

    config update

--- disentangle/configs/biosr_supervised_config.py
+++ disentangle/configs/biosr_supervised_config.py
@@ -55,8 +55,8 @@ def get_config():
     # If this is set to True, then target channels will be normalized from their separate mean.
     # otherwise, target will be normalized just the same way as the input, which is determined by use_one_mu_std
     data.target_separate_normalization = False
-    # data.ch1_min_alpha = 0.48
-    # data.ch1_max_alpha = 0.52
+    data.ch1_min_alpha = 0.47
+    data.ch1_max_alpha = 0.53
     data.variable_intensity_aug = False
     # data.variable_intensity_aug_scale_factor = 2
     # data.variable_intensity_aug_sigma = 0.2
