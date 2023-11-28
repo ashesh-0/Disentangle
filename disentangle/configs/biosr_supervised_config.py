f59306745d4bb07df8c5dcf49b67075e0898fbdd
Ashesh <ashesh276@gmail.com>
2023-11-28 15:28:49 +0100

    config update

--- disentangle/configs/biosr_supervised_config.py
+++ disentangle/configs/biosr_supervised_config.py
@@ -127,9 +127,9 @@ def get_config():
 
     training = config.training
     training.lr = 0.001 / 2
-    training.lr_scheduler_patience = int(30 / data.trainig_datausage_fraction if 'trainig_datausage_fraction' in
-                                         data else 30)
-    training.max_epochs = int(200 / data.trainig_datausage_fraction if 'trainig_datausage_fraction' in data else 200)
+    training.lr_scheduler_patience = int(90 / data.trainig_datausage_fraction if 'trainig_datausage_fraction' in
+                                         data else 90)
+    training.max_epochs = int(600 / data.trainig_datausage_fraction if 'trainig_datausage_fraction' in data else 600)
     training.batch_size = 128
     training.num_workers = 2
     training.val_repeat_factor = None
@@ -137,8 +137,8 @@ def get_config():
     training.val_fraction = 0.1
     training.test_fraction = 0.1
 
-    training.earlystop_patience = int(100 /
-                                      data.trainig_datausage_fraction if 'trainig_datausage_fraction' in data else 100)
+    training.earlystop_patience = int(300 /
+                                      data.trainig_datausage_fraction if 'trainig_datausage_fraction' in data else 300)
     training.precision = 16
     training.check_val_every_n_epoch = int(
         1 / (data.trainig_datausage_fraction)) if 'trainig_datausage_fraction' in data else None
