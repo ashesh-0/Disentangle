55613283150b081a354a30aabc82f477e755afcb
Ashesh <ashesh276@gmail.com>
2023-12-07 12:41:42 +0100

    notcorrelated idea

--- disentangle/loss/restricted_reconstruction_loss.py
+++ disentangle/loss/restricted_reconstruction_loss.py
@@ -22,11 +22,19 @@ class RestrictedReconstruction:
         return grad_direction
 
     @staticmethod
-    def get_grad_component(grad_vectors, reference_grad_directions, along_direction=False, orthogonal_direction=False):
+    def get_grad_component(grad_vectors,
+                           reference_grad_directions,
+                           along_direction=False,
+                           orthogonal_direction=False,
+                           not_positively_correlated=False):
         grad_components = []
-        assert along_direction != orthogonal_direction, 'Donot be lazy. Set either along_direction or orthogonal_direction to True.'
+        assert int(along_direction) + int(orthogonal_direction) + int(
+            not_positively_correlated
+        ) == 1, 'Donot be lazy. Set either along_direction or orthogonal_direction to True.'
         assert isinstance(along_direction, bool)
         assert isinstance(orthogonal_direction, bool)
+        assert isinstance(not_positively_correlated, bool)
+        neg_corr_count = 0
         for grad_vector, grad_direction in zip(grad_vectors, reference_grad_directions):
             if grad_vector is None:
                 grad_components.append(None)
@@ -36,6 +44,14 @@ class RestrictedReconstruction:
                     grad_components.append(grad_direction * component)
                 elif orthogonal_direction:
                     grad_components.append(grad_vector - grad_direction * component)
+                elif not_positively_correlated:
+                    if component > 0:
+                        grad_components.append(grad_vector - grad_direction * component)
+                    else:
+                        neg_corr_count += 1
+                        grad_components.append(grad_vector)
+
+        print('Retained neg corr fraction', neg_corr_count / len(grad_vectors))
 
         # check one grad for norm
         assert torch.norm(grad_direction) - 1 < 1e-6
@@ -57,8 +73,12 @@ class RestrictedReconstruction:
         if self._finegrained_restriction:
             correct_loss = self.loss_fn(normalized_target, normalized_target_prediction)
             correct_grad_all = self.get_grad_direction(correct_loss, params, retain_graph=retain_graph)
-            incorrect_c1_all = self.get_grad_component(incorrect_c1_all, correct_grad_all, orthogonal_direction=True)
-            incorrect_c2_all = self.get_grad_component(incorrect_c2_all, correct_grad_all, orthogonal_direction=True)
+            incorrect_c1_all = self.get_grad_component(incorrect_c1_all,
+                                                       correct_grad_all,
+                                                       not_positively_correlated=True)
+            incorrect_c2_all = self.get_grad_component(incorrect_c2_all,
+                                                       correct_grad_all,
+                                                       not_positively_correlated=True)
 
         incorrect_gradients = []
         for incorrect_c1, incorrect_c2 in zip(incorrect_c1_all, incorrect_c2_all):
@@ -82,7 +102,10 @@ class RestrictedReconstruction:
             assert isinstance(incor_grads, tuple) or isinstance(incor_grads, list)
             cor_grad = unsup_grad
             for incor_grad in incor_grads:
-                cor_grad -= torch.dot(cor_grad.view(-1, ), incor_grad.view(-1, )) * incor_grad
+                component = torch.dot(cor_grad.view(-1, ), incor_grad.view(-1, ))
+                if component > 0:
+                    cor_grad -= component * incor_grad
+
             output_grads.append(cor_grad)
         return output_grads, unsup_reconstruction_loss
 
@@ -113,3 +136,18 @@ class RestrictedReconstruction:
             param.grad = self._w_split * param.grad + self._w_recons * corrected_unsup_grad
 
         return {'input_reconstruction_loss': input_reconstruction_loss}
+
+
+if __name__ == '__main__':
+    direction_tensor = [torch.tensor([3.0, 4]), torch.tensor([1.0, 0, 0]), torch.tensor([12, 5.0])]
+    direction_tensor = [x / torch.norm(x) for x in direction_tensor]
+    grad_tensor = [torch.tensor([1, 2.0]), torch.tensor([5, 6, 1.0]), torch.tensor([1, 1.0])]
+    a = RestrictedReconstruction.get_grad_component(grad_tensor, direction_tensor, orthogonal_direction=True)
+    b = RestrictedReconstruction.get_grad_component(grad_tensor, direction_tensor, along_direction=True)
+    summed = [x + y for x, y in zip(a, b)]
+    dot = [torch.dot(x.view(-1), y.view(-1)) for x, y in zip(a, b)]
+    dot_along_direction = [torch.dot(x.view(-1), y.view(-1)) for x, y in zip(a, direction_tensor)]
+    print('dot along direction. should be 0', dot_along_direction)
+    print(summed)
+    print(grad_tensor)
+    print(dot)
