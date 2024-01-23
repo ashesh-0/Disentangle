fd21c8f6f6d8ddf6f6250183f295b431b0c8fb06
Ashesh <ashesh276@gmail.com>
2024-01-23 11:21:55 +0100

    config update

--- disentangle/configs/denoiser_config.py
+++ disentangle/configs/denoiser_config.py
@@ -36,6 +36,7 @@ def get_config():
     # If this is set to True, then target channels will be normalized from their separate mean.
     # otherwise, target will be normalized just the same way as the input, which is determined by use_one_mu_std
     data.target_separate_normalization = False
+    data.input_is_sum = False
 
     loss = config.loss
     loss.loss_type = LossType.Elbo
