96d2009277fa3e1d80b83084b530c76403e34827
Ashesh <ashesh276@gmail.com>
2024-02-04 22:19:42 +0100

    config update

--- disentangle/configs/hdn_denoiser_config.py
+++ disentangle/configs/hdn_denoiser_config.py
@@ -102,6 +102,7 @@ def get_config():
     fname_format = '/home/ashesh.ashesh/training/noise_model/{}/GMMNoiseModel_ventura_gigascience-{}__6_4_Clip0.001-0.999_Sig0.125_UpNone_Norm0_bootstrap.npz'
     model.noise_model_ch1_fpath = fname_format.format('2402/17', 'actin')
     model.noise_model_ch2_fpath = fname_format.format('2402/15', 'mito')
+    model.noise_model_learnable = True
     model.non_stochastic_version = False
 
     training = config.training
