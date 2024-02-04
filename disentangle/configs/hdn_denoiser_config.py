bc92c44c01e6eb1c892e31ae7f6f99b3d9a084e3
Ashesh <ashesh276@gmail.com>
2024-02-04 12:27:17 +0100

    denoiser config

--- disentangle/configs/hdn_denoiser_config.py
+++ disentangle/configs/hdn_denoiser_config.py
@@ -8,7 +8,7 @@ from disentangle.core.sampler_type import SamplerType
 def get_config():
     config = get_default_config()
     data = config.data
-    data.image_size = 256
+    data.image_size = 128
     data.data_type = DataType.SeparateTiffData
     data.channel_1 = 0
     data.channel_2 = 1
@@ -16,10 +16,15 @@ def get_config():
     data.ch2_fname = 'mito-60x-noise2-highsnr.tif'
     data.enable_poisson_noise = False
     data.enable_gaussian_noise = True
-    data.synthetic_gaussian_scale = 1500
+    # data.validtarget_random_fraction = 1.0
+    # data.training_validtarget_fraction = 0.2
+    config.data.synthetic_gaussian_scale = 1500
+    # if True, then input has 'identical' noise as the target. Otherwise, noise of input is independently sampled.
+    config.data.input_has_dependant_noise = True
 
     data.sampler_type = SamplerType.DefaultSampler
     data.threshold = 0.02
+    # data.grid_size = 1
     data.deterministic_grid = False
     data.normalized_input = True
     data.clip_percentile = 0.999
@@ -37,7 +42,6 @@ def get_config():
     # otherwise, target will be normalized just the same way as the input, which is determined by use_one_mu_std
     data.target_separate_normalization = True
     data.input_is_sum = False
-
     loss = config.loss
     loss.loss_type = LossType.Elbo
     # this is not uSplit.
@@ -48,39 +52,33 @@ def get_config():
     loss.kl_weight = 1.0
     loss.kl_annealing = False
     loss.kl_annealtime = 10
-    loss.kl_start = None
-    # loss.kl_min = 1e-7
+    loss.kl_start = -1
+    loss.kl_min = 1e-7
+    loss.free_bits = 1.0
 
     model = config.model
     model.model_type = ModelType.Denoiser
     # 4 values for denoise_channel {'Ch1', 'Ch2', 'input','all'}
     model.denoise_channel = 'Ch1'
+    model.z_dims = [128, 128, 128, 128]
 
     model.encoder.batchnorm = True
-    model.encoder.dropout = 0.2
+    model.encoder.blocks_per_layer = 1
+    model.encoder.n_filters = 64
+    model.encoder.dropout = 0.1
     model.encoder.res_block_kernel = 3
     model.encoder.res_block_skip_padding = False
 
     model.decoder.batchnorm = True
+    model.decoder.blocks_per_layer = 1
+    model.decoder.n_filters = 64
+    model.decoder.dropout = 0.1
     model.decoder.res_block_kernel = 3
     model.decoder.res_block_skip_padding = False
 
-    # HDN specific parameters which were changed.
-    model.decoder.dropout = 0.2
-    model.decoder.stochastic_use_naive_exponential = True
-    model.decoder.blocks_per_layer = 1
-    model.encoder.blocks_per_layer = 1
-    model.encoder.n_filters = 64
-    model.decoder.n_filters = 64
-    model.z_dims = [128, 128, 128, 128]
-    loss.free_bits = 1.0
-    model.analytical_kl = False
-    model.var_clip_max = None
-    model.logvar_lowerbound = None  # -2.49 is log(1/12), from paper "Re-parametrizing VAE for stablity."
-    model.monitor = 'val_loss'  # {'val_loss','val_psnr'}
-    #########################
+    #False
+    config.model.decoder.conv2d_bias = True
 
-    model.decoder.conv2d_bias = True
     model.skip_nboundary_pixels_from_loss = None
     model.nonlin = 'elu'
     model.merge_type = 'residual'
@@ -88,20 +86,29 @@ def get_config():
     model.learn_top_prior = True
     model.img_shape = None
     model.res_block_type = 'bacdbacd'
+
     model.gated = True
     model.no_initial_downscaling = True
+    model.analytical_kl = False
     model.mode_pred = False
-
+    model.var_clip_max = 20
     # predict_logvar takes one of the four values: [None,'global','channelwise','pixelwise']
-    model.predict_logvar = None  #'pixelwise'
+    model.predict_logvar = 'pixelwise'
+    model.logvar_lowerbound = -5  # -2.49 is log(1/12), from paper "Re-parametrizing VAE for stablity."
     model.multiscale_lowres_separate_branch = False
     model.multiscale_retain_spatial_dims = True
+    model.monitor = 'val_psnr'  # {'val_loss','val_psnr'}
 
-    model.enable_noise_model = True
+    model.enable_noise_model = False
     model.noise_model_type = 'gmm'
-    fname_format = '/home/ashesh.ashesh/training/noise_model/{}/GMMNoiseModel_ventura_gigascience-{}__6_4_Clip0.001-0.999_Sig0.125_UpNone_Norm0_bootstrap.npz'
-    model.noise_model_ch1_fpath = fname_format.format('2402/20', 'actin')
-    model.noise_model_ch2_fpath = fname_format.format('2402/19', 'mito')
+    # fname_format = '/home/ashesh.ashesh/training/noise_model/{}/GMMNoiseModel_ventura_gigascience-{}_6_4_Clip0.0-0.995_Sig0.125_UpNone_Norm1_bootstrap.npz'
+    model.noise_model_ch1_fpath = '/home/ashesh.ashesh/training/noise_model/2402/20/GMMNoiseModel_ventura_gigascience-actin__6_4_Clip0.001-0.999_Sig0.125_UpNone_Norm0_bootstrap.npz'
+    model.noise_model_ch2_fpath = '/home/ashesh.ashesh/training/noise_model/2402/19/GMMNoiseModel_ventura_gigascience-mito__6_4_Clip0.001-0.999_Sig0.125_UpNone_Norm0_bootstrap.npz'
+    # model.noise_model_learnable = False
+    assert model.enable_noise_model == False or model.predict_logvar is None
+
+    # model.noise_model_ch1_fpath = fname_format.format('2307/58', 'actin')
+    # model.noise_model_ch2_fpath = fname_format.format('2307/59', 'mito')
     model.non_stochastic_version = False
 
     training = config.training
