5263e2492bcc0385a1c92384c2f2e61ecf2ae58c
Ashesh <ashesh276@gmail.com>
2024-02-10 18:54:29 +0100

    config update

--- disentangle/configs/twotiff_config.py
+++ disentangle/configs/twotiff_config.py
@@ -14,7 +14,7 @@ def get_config():
     data.channel_2 = 1
     data.ch1_fname = 'actin-60x-noise2-highsnr.tif'
     data.ch2_fname = 'mito-60x-noise2-highsnr.tif'
-    data.poisson_noise_factor = -1
+    data.poisson_noise_factor = 100
     data.enable_gaussian_noise = True
     # data.validtarget_random_fraction = 1.0
     # data.training_validtarget_fraction = 0.2
