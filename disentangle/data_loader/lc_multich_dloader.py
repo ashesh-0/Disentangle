d8d2d7bc4c7131e58b052da5cbb97fcd636a04db
Ashesh <ashesh276@gmail.com>
2024-02-10 16:42:52 +0100

    LC enabled with noise

--- disentangle/data_loader/lc_multich_dloader.py
+++ disentangle/data_loader/lc_multich_dloader.py
@@ -64,6 +64,8 @@ class LCMultiChDloader(MultiChDloader):
         self.num_scales = num_scales
         assert self.num_scales is not None
         self._scaled_data = [self._data]
+        self._scaled_noise_data = [self._noise_data]
+
         assert isinstance(self.num_scales, int) and self.num_scales >= 1
         self._lowres_supervision = lowres_supervision
         assert isinstance(self._padding_kwargs, dict)
@@ -75,6 +77,9 @@ class LCMultiChDloader(MultiChDloader):
             new_shape = (shape[0], shape[1] // 2, shape[2] // 2, shape[3])
             ds_data = resize(self._scaled_data[-1], new_shape)
             self._scaled_data.append(ds_data)
+            # do the same for noise
+            noise_data = resize(self._scaled_noise_data[-1], new_shape)
+            self._scaled_noise_data.append(noise_data)
 
     def _init_msg(self):
         msg = super()._init_msg()
@@ -87,7 +92,15 @@ class LCMultiChDloader(MultiChDloader):
         else:
             idx, _ = index
         imgs = self._scaled_data[scaled_index][idx % self.N]
-        return tuple([imgs[None, :, :, i] for i in range(imgs.shape[-1])])
+        imgs = tuple([imgs[None, :, :, i] for i in range(imgs.shape[-1])])
+        if self._noise_data is not None:
+            noisedata = self._scaled_noise_data[scaled_index][idx % self.N]
+            noise = tuple([noisedata[None, :, :, i] for i in range(noisedata.shape[-1])])
+            factor = np.sqrt(2) if self._input_is_sum else 1.0
+            # since we are using this lowres images for just the input, we need to add the noise of the input.
+            assert self._lowres_supervision is False
+            imgs = tuple([img + noise[0] * factor for img in imgs])
+        return imgs
 
     def _crop_img(self, img: np.ndarray, h_start: int, w_start: int):
         """
@@ -100,7 +113,7 @@ class LCMultiChDloader(MultiChDloader):
         """
         Returns the primary patch along with low resolution patches centered on the primary patch.
         """
-        img_tuples = self._load_img(index)
+        img_tuples, noise_tuples = self._load_img(index)
         assert self._img_sz is not None
         h, w = img_tuples[0].shape[-2:]
         if self._enable_random_cropping:
@@ -109,7 +122,7 @@ class LCMultiChDloader(MultiChDloader):
             h_start, w_start = self._get_deterministic_hw(index)
 
         cropped_img_tuples = [self._crop_flip_img(img, h_start, w_start, False, False) for img in img_tuples]
-
+        cropped_noise_tuples = [self._crop_flip_img(noise, h_start, w_start, False, False) for noise in noise_tuples]
         h_center = h_start + self._img_sz // 2
         w_center = w_start + self._img_sz // 2
         allres_versions = {i: [cropped_img_tuples[i]] for i in range(len(cropped_img_tuples))}
@@ -129,18 +142,29 @@ class LCMultiChDloader(MultiChDloader):
                 allres_versions[ch_idx].append(scaled_cropped_img_tuples[ch_idx])
 
         output_img_tuples = tuple([np.concatenate(allres_versions[ch_idx]) for ch_idx in range(len(img_tuples))])
-        return output_img_tuples
+        return output_img_tuples, cropped_noise_tuples
 
     def __getitem__(self, index: Union[int, Tuple[int, int]]):
-        img_tuples = self._get_img(index)
+        img_tuples, noise_tuples = self._get_img(index)
         assert self._enable_rotation is False
 
-        if self._lowres_supervision:
-            target = np.concatenate([img[:, None] for img in img_tuples], axis=1)
+        assert self._lowres_supervision != True
+        if len(noise_tuples) > 0:
+            target = np.concatenate([img[:1] + noise for img, noise in zip(img_tuples, noise_tuples)], axis=0)
         else:
             target = np.concatenate([img[:1] for img in img_tuples], axis=0)
 
-        inp, alpha = self._compute_input(img_tuples)
+        # add noise to input
+        if len(noise_tuples) > 0:
+            factor = np.sqrt(2) if self._input_is_sum else 1.0
+            input_tuples = []
+            for x in img_tuples:
+                x[0] = x[0] + noise_tuples[0] * factor
+                input_tuples.append(x)
+        else:
+            input_tuples = img_tuples
+
+        inp, alpha = self._compute_input(input_tuples)
 
         output = [inp, target]
 
@@ -159,3 +183,42 @@ class LCMultiChDloader(MultiChDloader):
 
         # _, grid_size = index
         # return inp, target, grid_size
+
+
+if __name__ == '__main__':
+    # from disentangle.configs.microscopy_multi_channel_lvae_config import get_config
+    import matplotlib.pyplot as plt
+
+    from disentangle.configs.twotiff_config import get_config
+    config = get_config()
+    padding_kwargs = {'mode': config.data.padding_mode}
+    if 'padding_value' in config.data and config.data.padding_value is not None:
+        padding_kwargs['constant_values'] = config.data.padding_value
+
+    dset = LCMultiChDloader(config.data,
+                            '/group/jug/ashesh/data/ventura_gigascience_small/',
+                            DataSplitType.Train,
+                            val_fraction=config.training.val_fraction,
+                            test_fraction=config.training.test_fraction,
+                            normalized_input=config.data.normalized_input,
+                            enable_rotation_aug=config.data.train_aug_rotate,
+                            enable_random_cropping=config.data.deterministic_grid is False,
+                            use_one_mu_std=config.data.use_one_mu_std,
+                            allow_generation=False,
+                            num_scales=config.data.multiscale_lowres_count,
+                            max_val=None,
+                            padding_kwargs=padding_kwargs,
+                            grid_alignment=GridAlignement.LeftTop,
+                            overlapping_padding_kwargs=None)
+
+    mean, std = dset.compute_mean_std()
+    dset.set_mean_std(mean, std)
+
+    inp, tar = dset[0]
+    print(inp.shape, tar.shape)
+    _, ax = plt.subplots(figsize=(10, 2), ncols=5)
+    ax[0].imshow(inp[0])
+    ax[1].imshow(inp[1])
+    ax[2].imshow(inp[2])
+    ax[3].imshow(tar[0])
+    ax[4].imshow(tar[1])
