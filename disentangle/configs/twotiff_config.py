bf6c16ea5c178152a5b7cba7804548f2882bbf51
Ashesh <ashesh276@gmail.com>
2024-02-11 23:14:27 +0100

    config update

--- disentangle/configs/twotiff_config.py
+++ disentangle/configs/twotiff_config.py
@@ -15,7 +15,7 @@ def get_config():
     data.ch1_fname = 'actin-60x-noise2-highsnr.tif'
     data.ch2_fname = 'mito-60x-noise2-highsnr.tif'
     data.poisson_noise_factor = -1
-    data.enable_gaussian_noise = False
+    data.enable_gaussian_noise = True
     # data.validtarget_random_fraction = 1.0
     # data.training_validtarget_fraction = 0.2
     config.data.synthetic_gaussian_scale = 1000
@@ -99,9 +99,10 @@ def get_config():
 
     model.enable_noise_model = True
     model.noise_model_type = 'gmm'
-    # fname_format = '/home/ashesh.ashesh/training/noise_model/{}/GMMNoiseModel_ventura_gigascience-{}_6_4_Clip0.0-0.995_Sig0.125_UpNone_Norm1_bootstrap.npz'
-    model.noise_model_ch1_fpath = '/home/ashesh.ashesh/training/noise_model/2402/128/GMMNoiseModel_ventura_gigascience-actin__6_4_Clip0.0-1.0_Sig0.125_UpNone_Norm0_bootstrap.npz'
-    model.noise_model_ch2_fpath = '/home/ashesh.ashesh/training/noise_model/2402/129/GMMNoiseModel_ventura_gigascience-mito__6_4_Clip0.0-1.0_Sig0.125_UpNone_Norm0_bootstrap.npz'
+    fname_format = '/home/ashesh.ashesh/training/noise_model/{}/GMMNoiseModel_ventura_gigascience-{}__6_4_Clip0.0-1.0_Sig0.125_UpNone_Norm0_bootstrap.npz'
+    model.noise_model_ch1_fpath = fname_format.format('2402/139', 'actin')
+    model.noise_model_ch2_fpath = fname_format.format('2402/138', 'mito')
+
     model.noise_model_learnable = False
     assert model.enable_noise_model == False or model.predict_logvar is None
 
