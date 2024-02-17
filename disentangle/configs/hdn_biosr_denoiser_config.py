7331351f08a5901e041a2edc5ba2d989efa3badf
Ashesh <ashesh276@gmail.com>
2024-02-17 18:26:44 +0100

    config update

--- disentangle/configs/hdn_biosr_denoiser_config.py
+++ disentangle/configs/hdn_biosr_denoiser_config.py
@@ -8,7 +8,7 @@ from disentangle.core.sampler_type import SamplerType
 def get_config():
     config = get_default_config()
     data = config.data
-    data.image_size = 256
+    data.image_size = 128
     data.data_type = DataType.BioSR_MRC
     data.channel_1 = 0
     data.channel_2 = 1
@@ -109,7 +109,7 @@ def get_config():
     training.lr = 0.001
     training.lr_scheduler_patience = 15
     training.max_epochs = 200
-    training.batch_size = 8
+    training.batch_size = 32
     training.num_workers = 4
     training.val_repeat_factor = None
     training.train_repeat_factor = None
