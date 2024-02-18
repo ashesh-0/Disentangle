99ace8c80920b0ffbf9b28f123beff08c8337dfa
Ashesh <ashesh276@gmail.com>
2024-02-18 17:42:20 +0100

    config update

--- disentangle/configs/hdn_biosr_denoiser_config.py
+++ disentangle/configs/hdn_biosr_denoiser_config.py
@@ -12,8 +12,8 @@ def get_config():
     data.data_type = DataType.BioSR_MRC
     data.channel_1 = 0
     data.channel_2 = 1
-    data.ch1_fname = 'CCPs/GT_all.mrc'
-    data.ch2_fname = 'ER/GT_all.mrc'
+    data.ch1_fname = 'ER/GT_all.mrc'
+    data.ch2_fname = 'Microtubules/GT_all.mrc'
     data.poisson_noise_factor = -1
     data.enable_gaussian_noise = True
     data.synthetic_gaussian_scale = 5100
