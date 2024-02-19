d8a83ccc6c077db71fdb5dd92a196bcb9c7548b6
Ashesh <ashesh276@gmail.com>
2024-02-19 19:06:52 +0100

    config update

--- disentangle/configs/biosr_config.py
+++ disentangle/configs/biosr_config.py
@@ -101,7 +101,7 @@ def get_config():
     model.monitor = 'val_loss'  # {'val_loss','val_psnr'}
 
     model.enable_noise_model = True
-    model.noise_model_type = 'histgmm'
+    model.noise_model_type = 'gmm'
     # fname_format = '/home/ashesh.ashesh/training/noise_model/{}/GMMNoiseModel_ventura_gigascience-{}_6_4_Clip0.0-0.995_Sig0.125_UpNone_Norm1_bootstrap.npz'
     model.noise_model_ch1_fpath = '/home/ashesh.ashesh/training/noise_model/2402/336/HistNoiseModel_BioSR-ER_GT_all__Norm0_Bins128_bootstrap.npy'
     model.noise_model_ch2_fpath = '/home/ashesh.ashesh/training/noise_model/2402/338/HistNoiseModel_BioSR-Microtubules_GT_all__Norm0_Bins128_bootstrap.npy'
