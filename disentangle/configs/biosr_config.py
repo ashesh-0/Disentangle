d544b2cdac70ba2a52462d2ba41be49eba4b8cb5
ashesh <ashesh276@gmail.com>
2024-02-26 16:19:40 +0000

    config update

--- disentangle/configs/biosr_config.py
+++ disentangle/configs/biosr_config.py
@@ -94,7 +94,7 @@ def get_config():
     model.mode_pred = False
     model.var_clip_max = 20
     # predict_logvar takes one of the four values: [None,'global','channelwise','pixelwise']
-    model.predict_logvar = 'channelwise'
+    model.predict_logvar = 'pixelwise'
     model.logvar_lowerbound = -5  # -2.49 is log(1/12), from paper "Re-parametrizing VAE for stablity."
     model.multiscale_lowres_separate_branch = False
     model.multiscale_retain_spatial_dims = True
