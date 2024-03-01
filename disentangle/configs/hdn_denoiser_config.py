00e7e760c78656267ca7fc753c2510ff12a0894a
Ashesh <ashesh276@gmail.com>
2024-03-01 14:01:42 +0100

    config update

--- disentangle/configs/hdn_denoiser_config.py
+++ disentangle/configs/hdn_denoiser_config.py
@@ -12,10 +12,10 @@ def get_config():
     data.data_type = DataType.SeparateTiffData
     data.channel_1 = 0
     data.channel_2 = 1
-    data.ch1_fname = 'actin-60x-noise2-highsnr.tif'
-    data.ch2_fname = 'mito-60x-noise2-highsnr.tif'
+    data.ch1_fname = 'actin-60x-noise2-lowsnr.tif'
+    data.ch2_fname = 'mito-60x-noise2-lowsnr.tif'
     data.poisson_noise_factor = -1
-    data.enable_gaussian_noise = True
+    data.enable_gaussian_noise = False
     data.synthetic_gaussian_scale = 250
 
     data.sampler_type = SamplerType.DefaultSampler
@@ -101,8 +101,8 @@ def get_config():
     model.enable_noise_model = True
     model.noise_model_type = 'gmm'
     fname_format = '/home/ashesh.ashesh/training/noise_model/{}/GMMNoiseModel_ventura_gigascience-{}__6_4_Clip0.0-1.0_Sig0.125_UpNone_Norm0_bootstrap.npz'
-    model.noise_model_ch1_fpath = fname_format.format('2402/269', 'actin')
-    model.noise_model_ch2_fpath = fname_format.format('2402/270', 'mito')
+    model.noise_model_ch1_fpath = '/home/ashesh.ashesh/training/noise_model/2402/420/GMMNoiseModel_ventura_gigascience-actin__6_4_Clip0.0-1.0_Sig0.125_UpNone_Norm0_bootstrap.npz'
+    model.noise_model_ch2_fpath = '/home/ashesh.ashesh/training/noise_model/2402/419/GMMNoiseModel_ventura_gigascience-mito__6_4_Clip0.0-1.0_Sig0.125_UpNone_Norm0_bootstrap.npz'
     model.noise_model_learnable = False
     model.non_stochastic_version = False
 
