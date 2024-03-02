90d105764417e3774598aa6eff751b923553e024
Ashesh <ashesh276@gmail.com>
2024-03-02 11:54:52 +0100

    script to save results

--- disentangle/scripts/evaluate_sequentially.py
+++ disentangle/scripts/evaluate_sequentially.py
@@ -7,8 +7,12 @@ if __name__ == '__main__':
     parser.add_argument('--normalized_ssim', action='store_true')
     parser.add_argument('--save_prediction', action='store_true')
     parser.add_argument('--mmse_count', type=int, default=1)
+    parser.add_argument('--start_k', type=int, default=0)
+    parser.add_argument('--end_k', type=int, default=1000)
+
     args = parser.parse_args()
-    for i in range(30, 5000):
+    print('Evaluating between', args.start_k, args.end_k)
+    for i in range(args.start_k, args.end_k):
         print('')
         print('##################################')
         print(f'Predicting {i}th frame')
