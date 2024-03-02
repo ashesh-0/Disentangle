b23b4d3713760928ee0e3aaf291dc4ff77aa797e
Ashesh <ashesh276@gmail.com>
2024-03-02 13:07:36 +0100

    config update

--- disentangle/configs/hdn_denoiser_config.py
+++ disentangle/configs/hdn_denoiser_config.py
@@ -54,7 +54,7 @@ def get_config():
     model = config.model
     model.model_type = ModelType.Denoiser
     # 4 values for denoise_channel {'Ch1', 'Ch2', 'input','all'}
-    model.denoise_channel = 'Ch1'
+    model.denoise_channel = 'input'
 
     model.encoder.batchnorm = True
     model.encoder.res_block_kernel = 3
@@ -101,7 +101,7 @@ def get_config():
     model.enable_noise_model = True
     model.noise_model_type = 'gmm'
     fname_format = '/home/ashesh.ashesh/training/noise_model/{}/GMMNoiseModel_ventura_gigascience-{}__6_4_Clip0.0-1.0_Sig0.125_UpNone_Norm0_bootstrap.npz'
-    model.noise_model_ch1_fpath = '/home/ashesh.ashesh/training/noise_model/2402/516/GMMNoiseModel_ventura_gigascience-actin__6_4_Clip0.0-1.0_Sig0.125_UpNone_Norm0_bootstrap.npz'
+    model.noise_model_ch1_fpath = '/home/ashesh.ashesh/training/noise_model/2402/517/GMMNoiseModel_ventura_gigascience-mito_actin_6_4_Clip0.0-1.0_Sig0.125_UpNone_Norm0_bootstrap.npz'
     model.noise_model_ch2_fpath = '/home/ashesh.ashesh/training/noise_model/2402/515/GMMNoiseModel_ventura_gigascience-mito__6_4_Clip0.0-1.0_Sig0.125_UpNone_Norm0_bootstrap.npz'
     model.noise_model_learnable = False
     model.non_stochastic_version = False
