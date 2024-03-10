f27ef45ca87890eaadfdf1906cefcdcef7fb7b74
Ashesh <ashesh276@gmail.com>
2024-03-10 15:11:15 +0100

    config update

--- disentangle/configs/biosr_config.py
+++ disentangle/configs/biosr_config.py
@@ -13,7 +13,7 @@ def get_config():
     # data.channel_1 = 0
     # data.channel_2 = 1
     data.ch1_fname = 'F-actin/GT_all_a.mrc'
-    data.ch2_fname = 'Microtubules/GT_all.mrc'
+    data.ch2_fname = 'CCPs/GT_all.mrc'
     data.num_channels = 2
 
     data.poisson_noise_factor = -1
@@ -22,7 +22,7 @@ def get_config():
     data.trainig_datausage_fraction = 1.0
     # data.validtarget_random_fraction = 1.0
     # data.training_validtarget_fraction = 0.2
-    config.data.synthetic_gaussian_scale = 4300
+    config.data.synthetic_gaussian_scale = 3050
     # if True, then input has 'identical' noise as the target. Otherwise, noise of input is independently sampled.
     config.data.input_has_dependant_noise = True
 
@@ -96,13 +96,13 @@ def get_config():
     model.mode_pred = False
     model.var_clip_max = 20
     # predict_logvar takes one of the four values: [None,'global','channelwise','pixelwise']
-    model.predict_logvar = None
+    model.predict_logvar = 'pixelwise'
     model.logvar_lowerbound = -5  # -2.49 is log(1/12), from paper "Re-parametrizing VAE for stablity."
     model.multiscale_lowres_separate_branch = False
     model.multiscale_retain_spatial_dims = True
     model.monitor = 'val_loss'  # {'val_loss','val_psnr'}
 
-    model.enable_noise_model = True
+    model.enable_noise_model = False
     model.noise_model_type = 'gmm'
     fname = '/home/ashesh.ashesh/training/noise_model/2403/14/GMMNoiseModel_BioSR-__6_4_Clip0.0-1.0_Sig0.125_UpNone_Norm0_bootstrap.npz'
     model.noise_model_ch1_fpath = '/home/ashesh.ashesh/training/noise_model/2403/64/GMMNoiseModel_BioSR-F__6_4_Clip0.0-1.0_Sig0.125_UpNone_Norm0_bootstrap.npz'
