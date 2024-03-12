3bb2ce08f1e8f0380bab30ad853f736704b623b8
Ashesh <ashesh276@gmail.com>
2024-03-12 12:03:02 +0100

    config update

--- disentangle/configs/biosr_config.py
+++ disentangle/configs/biosr_config.py
@@ -12,7 +12,7 @@ def get_config():
     data.data_type = DataType.BioSR_MRC
     # data.channel_1 = 0
     # data.channel_2 = 1
-    data.ch1_fname = 'F-actin/GT_all_a.mrc'
+    data.ch1_fname = 'ER/GT_all.mrc'
     data.ch2_fname = 'CCPs/GT_all.mrc'
     data.num_channels = 2
 
@@ -22,7 +22,7 @@ def get_config():
     data.trainig_datausage_fraction = 1.0
     # data.validtarget_random_fraction = 1.0
     # data.training_validtarget_fraction = 0.2
-    config.data.synthetic_gaussian_scale = 3050
+    config.data.synthetic_gaussian_scale = 3400
     # if True, then input has 'identical' noise as the target. Otherwise, noise of input is independently sampled.
     config.data.input_has_dependant_noise = True
 
@@ -104,9 +104,9 @@ def get_config():
 
     model.enable_noise_model = True
     model.noise_model_type = 'gmm'
-    fname = '/home/ashesh.ashesh/training/noise_model/2403/14/GMMNoiseModel_BioSR-__6_4_Clip0.0-1.0_Sig0.125_UpNone_Norm0_bootstrap.npz'
-    model.noise_model_ch1_fpath = '/home/ashesh.ashesh/training/noise_model/2403/64/GMMNoiseModel_BioSR-F__6_4_Clip0.0-1.0_Sig0.125_UpNone_Norm0_bootstrap.npz'
-    model.noise_model_ch2_fpath = '/home/ashesh.ashesh/training/noise_model/2403/65/GMMNoiseModel_BioSR-Microtubules_GT_all__6_4_Clip0.0-1.0_Sig0.125_UpNone_Norm0_bootstrap.npz'
+    fname = '/home/ashesh.ashesh/training/noise_model/2403/139/GMMNoiseModel_BioSR-__6_4_Clip0.0-1.0_Sig0.125_UpNone_Norm0_bootstrap.npz'
+    model.noise_model_ch1_fpath = fname
+    model.noise_model_ch2_fpath = fname
     model.noise_model_learnable = False
     assert model.enable_noise_model == False or model.predict_logvar is None
 
