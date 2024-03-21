54221151ec7187b050c0bf3bd3e602dacf653cbc
Ashesh <ashesh276@gmail.com>
2024-03-21 14:18:57 +0100

    config update

--- disentangle/configs/pavia3_config.py
+++ disentangle/configs/pavia3_config.py
@@ -9,14 +9,14 @@ from disentangle.data_loader.pavia3_rawdata_loader import Pavia3SeqAlpha, Pavia3
 def get_config():
     config = get_default_config()
     data = config.data
-    data.image_size = 128
+    data.image_size = 64
     data.data_type = DataType.Pavia3SeqData
     # data.channel_1 = 0
     # data.channel_2 = 1
     # data.ch1_fname = 'ER/GT_all.mrc'
     # data.ch2_fname = 'Microtubules/GT_all.mrc'
     data.num_channels = 2
-    data.power_level = Pavia3SeqPowerLevel.Low
+    data.power_level = Pavia3SeqPowerLevel.Medium
     data.alpha_level = Pavia3SeqAlpha.Balanced
 
     data.enable_gaussian_noise = False
@@ -41,7 +41,7 @@ def get_config():
     data.use_one_mu_std = True
     data.train_aug_rotate = False
     data.randomized_channels = False
-    data.multiscale_lowres_count = None
+    data.multiscale_lowres_count = 5
     data.padding_mode = 'reflect'
     data.padding_value = None
     # If this is set to True, then target channels will be normalized from their separate mean.
