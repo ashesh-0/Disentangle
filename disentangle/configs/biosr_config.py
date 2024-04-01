485865cae23fdc60da209e24c0919b5e2fcdf3fb
Ashesh <ashesh276@gmail.com>
2024-04-01 22:50:53 +0200

    config update

--- disentangle/configs/biosr_config.py
+++ disentangle/configs/biosr_config.py
@@ -117,7 +117,7 @@ def get_config():
     training = config.training
     training.lr = 0.001
     training.lr_scheduler_patience = 30
-    training.max_epochs = 200
+    training.max_epochs = 400
     training.batch_size = 32
     training.num_workers = 4
     training.val_repeat_factor = None
