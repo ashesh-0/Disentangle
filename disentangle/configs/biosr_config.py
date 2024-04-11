01d5cbb52284b359a88dce8fb723a05a65b3e147
Ashesh <ashesh276@gmail.com>
2024-04-11 16:38:59 +0200

    config update

--- disentangle/configs/biosr_config.py
+++ disentangle/configs/biosr_config.py
@@ -39,7 +39,7 @@ def get_config():
     data.use_one_mu_std = True
     data.train_aug_rotate = False
     data.randomized_channels = False
-    data.multiscale_lowres_count = None
+    data.multiscale_lowres_count = 3
     data.padding_mode = 'reflect'
     data.padding_value = None
     # If this is set to True, then target channels will be normalized from their separate mean.
@@ -126,5 +126,5 @@ def get_config():
     training.val_fraction = 0.1
     training.test_fraction = 0.1
     training.earlystop_patience = 200
-    # training.precision = 16
+    training.precision = 16
     return config
