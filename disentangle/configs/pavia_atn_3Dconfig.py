bef732892e06a488ffd2e068d76037612bef6538
ashesh <ashesh276@gmail.com>
2024-04-15 10:40:57 +0000

    config update

--- disentangle/configs/pavia_atn_3Dconfig.py
+++ disentangle/configs/pavia_atn_3Dconfig.py
@@ -16,7 +16,7 @@ def get_config():
     data.data_type = DataType.OptiMEM100_014
     data.channel_1 = 2
     data.channel_2 = 3
-    data.depth3D = 3
+    data.depth3D = 6
 
     data.poisson_noise_factor = -1
     data.enable_gaussian_noise = False
@@ -39,7 +39,7 @@ def get_config():
     data.use_one_mu_std = True
     data.train_aug_rotate = True
     data.randomized_channels = False
-    data.multiscale_lowres_count = 5
+    data.multiscale_lowres_count = 3
     data.padding_mode = 'reflect'
     data.padding_value = None
     # If this is set to True, then target channels will be normalized from their separate mean.
