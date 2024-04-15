aa67ce6136c0e03115a2d293153456bdd262c2c7
Ashesh <ashesh276@gmail.com>
2024-04-15 12:44:11 +0200

    notebook changes

--- disentangle/analysis/paper_plots.py
+++ disentangle/analysis/paper_plots.py
@@ -13,12 +13,9 @@ from disentangle.core.psnr import RangeInvariantPsnr
 
 def get_plotoutput_dir(ckpt_dir, patch_size, mmse_count=50):
     plotsrootdir = f'/group/jug/ashesh/data/paper_figures/patch_{patch_size}_mmse_{mmse_count}'
-    rdate, rconfig, rid = ckpt_dir.split("/")[-3:]
-    fname_prefix = rdate + '-' + rconfig.replace('-', '')[:-2] + '-' + rid
-    plotsdir = os.path.join(plotsrootdir, fname_prefix)
-    os.makedirs(plotsdir, exist_ok=True)
-    print(plotsdir)
-    return plotsdir
+    os.makedirs(plotsrootdir, exist_ok=True)
+    print(plotsrootdir)
+    return plotsrootdir
 
 
 def get_last_index(bin_count, quantile):
