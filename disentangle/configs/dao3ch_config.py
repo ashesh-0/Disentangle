c4cfadafaf626553338025d3a683453bd8347131
Ashesh <ashesh276@gmail.com>
2024-04-15 22:26:09 +0200

    config update

--- disentangle/configs/dao3ch_config.py
+++ disentangle/configs/dao3ch_config.py
@@ -16,56 +16,57 @@ def get_config():
     data.image_size = 64
     data.data_type = DataType.Dao3Channel
     data.subdset_type = SubDsetType.MultiChannel
-    data.num_channels = 3
+    data.channel_1 = 0
+    data.channel_2 = 1
+    data.num_channels = 2
+    data.uncorrelated_channels = True
+
+    data.poisson_noise_factor = -1
+
+    data.enable_gaussian_noise = False
+    data.trainig_datausage_fraction = 1.0
+    # data.validtarget_random_fraction = 1.0
+    # data.training_validtarget_fraction = 0.2
+    config.data.synthetic_gaussian_scale = 6675
+    # if True, then input has 'identical' noise as the target. Otherwise, noise of input is independently sampled.
+    config.data.input_has_dependant_noise = True
 
     data.sampler_type = SamplerType.DefaultSampler
+    data.threshold = 0.02
+    # data.grid_size = 1
     data.deterministic_grid = False
     data.normalized_input = True
-    data.clip_percentile = 0.995
-    data.background_quantile = 0.0
-    # With background quantile, one is setting the avg background value to 0. With this, any negative values are also set to 0.
-    # This, together with correct background_quantile should altogether get rid of the background. The issue here is that
-    # the background noise is also a distribution. So, some amount of background noise will remain.
-    data.clip_background_noise_to_zero = False
+    data.clip_percentile = 1
 
-    # we will not subtract the mean of the dataset from every patch. We just want to subtract the background and normalize using std. This way, background will be very close to 0.
-    # this will help in the all scaling related approaches where we want to multiply the frame with some factor and then add them. we will then effectively just do these scaling on the
-    # foreground pixels and the background will anyways will remain very close to 0.
-    data.skip_normalization_using_mean = False
-
-    data.input_is_sum = False
-
+    data.channelwise_quantile = False
     # If this is set to true, then one mean and stdev is used for both channels. Otherwise, two different
     # meean and stdev are used.
     data.use_one_mu_std = True
     data.train_aug_rotate = False
     data.randomized_channels = False
-    data.multiscale_lowres_count = 5
+    data.multiscale_lowres_count = None
     data.padding_mode = 'reflect'
     data.padding_value = None
     # If this is set to True, then target channels will be normalized from their separate mean.
     # otherwise, target will be normalized just the same way as the input, which is determined by use_one_mu_std
-    data.target_separate_normalization = False
-
-    # This is for intensity augmentation
-    data.ch1_min_alpha = 0.4
-    data.ch1_max_alpha = 0.6
-    data.alpha_weighted_target = True
-    # data.return_alpha = True
+    data.target_separate_normalization = True
+    data.input_is_sum = False
 
     loss = config.loss
-    loss.loss_type = LossType.Elbo
-    loss.kl_loss_formulation = 'usplit'
-    # loss.mixed_rec_weight = 1
+    loss.loss_type = LossType.DenoiSplitMuSplit
+    loss.usplit_w = 0
+    loss.denoisplit_w = 1 - loss.usplit_w
+    loss.kl_loss_formulation = 'denoisplit_usplit'
 
+    # loss.mixed_rec_weight = 1
+    loss.restricted_kl = False
     loss.kl_weight = 1.0
+    loss.reconstruction_weight = 1.0
     loss.kl_annealing = False
     loss.kl_annealtime = 10
     loss.kl_start = -1
     loss.kl_min = 1e-7
-    loss.free_bits = 0.0
-    # loss.ch1_recons_w = 1
-    # loss.ch2_recons_w = 5
+    loss.free_bits = 1.0
 
     model = config.model
     model.model_type = ModelType.LadderVae
@@ -102,21 +103,29 @@ def get_config():
     model.mode_pred = False
     model.var_clip_max = 20
     # predict_logvar takes one of the four values: [None,'global','channelwise','pixelwise']
-    model.predict_logvar = 'pixelwise'
+    model.predict_logvar = 'pixelwise'  #'pixelwise' #'channelwise'
     model.logvar_lowerbound = -5  # -2.49 is log(1/12), from paper "Re-parametrizing VAE for stablity."
     model.multiscale_lowres_separate_branch = False
     model.multiscale_retain_spatial_dims = True
-    model.monitor = 'val_psnr'  # {'val_loss','val_psnr'}
+    model.monitor = 'val_loss'  # {'val_loss','val_psnr'}
+
+    model.enable_noise_model = True
+    model.noise_model_type = 'gmm'
+    fname = '/home/ubuntu/ashesh/training_hpc/noise_model/2404/41/GMMNoiseModel_BioSR-__6_4_Clip0.0-1.0_Sig0.125_UpNone_Norm0_bootstrap.npz'
+    model.noise_model_ch1_fpath = fname
+    model.noise_model_ch2_fpath = fname
+
+    model.noise_model_learnable = False
+    # assert model.enable_noise_model == False or model.predict_logvar is None
+    # model.noise_model_ch1_fpath = fname_format.format('2307/58', 'actin')
+    # model.noise_model_ch2_fpath = fname_format.format('2307/59', 'mito')
     model.non_stochastic_version = False
-    model.enable_noise_model = False
-    model.noise_model_ch1_fpath = None
-    model.noise_model_ch1_fpath = None
 
     training = config.training
-    training.lr = 0.001 / 2
+    training.lr = 0.001
     training.lr_scheduler_patience = 30
     training.max_epochs = 400
-    training.batch_size = 16
+    training.batch_size = 32
     training.num_workers = 4
     training.val_repeat_factor = None
     training.train_repeat_factor = None
@@ -124,5 +133,5 @@ def get_config():
     training.test_fraction = 0.1
     training.earlystop_patience = 200
     training.precision = 16
-
     return config
+
