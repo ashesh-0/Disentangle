64cb6f0871a293fafa9eb20d9ec9f15ec3470982
Ashesh <ashesh276@gmail.com>
2024-04-16 14:33:44 +0200

    config update

--- disentangle/configs/dao3ch_config.py
+++ disentangle/configs/dao3ch_config.py
@@ -133,6 +133,6 @@ def get_config():
     training.test_fraction = 0.1
     training.earlystop_patience = 120
     training.precision = 16
-    training.limit_train_batches=1000
+    training.limit_train_batches=2000
     return config
 
