b2aafc5aa12dc4d1fc3c454f368b3f32c6e97145
Ashesh <ashesh276@gmail.com>
2024-04-25 16:58:53 +0200

    config update

--- disentangle/configs/exp_microscopyv2_config.py
+++ disentangle/configs/exp_microscopyv2_config.py
@@ -13,7 +13,7 @@ from disentangle.data_loader.multifile_raw_dloader import SubDsetType
 def get_config():
     config = get_default_config()
     data = config.data
-    data.image_size = 128
+    data.image_size = 64
     data.data_type = DataType.ExpMicroscopyV2
     data.subdset_type = SubDsetType.MultiChannel
     data.num_channels = 2
@@ -40,7 +40,7 @@ def get_config():
     data.use_one_mu_std = True
     data.train_aug_rotate = False
     data.randomized_channels = False
-    data.multiscale_lowres_count = 2
+    data.multiscale_lowres_count = 3
     data.padding_mode = 'reflect'
     data.padding_value = None
     # If this is set to True, then target channels will be normalized from their separate mean.
@@ -48,24 +48,27 @@ def get_config():
     data.target_separate_normalization = False
 
     # This is for intensity augmentation
-    data.ch1_min_alpha = 0.4
-    data.ch1_max_alpha = 0.6
-    data.alpha_weighted_target = True
+    # data.ch1_min_alpha = 0.4
+    # data.ch1_max_alpha = 0.6
+    # data.alpha_weighted_target = True
     # data.return_alpha = True
 
     loss = config.loss
-    loss.loss_type = LossType.Elbo
-    loss.kl_loss_formulation = 'usplit'
+    loss.loss_type = LossType.DenoiSplitMuSplit
+    # this is not uSplit.
+    loss.kl_loss_formulation = 'denoisplit_usplit'
+    loss.usplit_w = 0.1
+    loss.denoisplit_w = 1 - loss.usplit_w
+    loss.restricted_kl = True
+    loss.reconstruction_weight = 1.0
     # loss.mixed_rec_weight = 1
 
-    loss.kl_weight = 50.0
+    loss.kl_weight = 1.0
     loss.kl_annealing = False
     loss.kl_annealtime = 10
     loss.kl_start = -1
     loss.kl_min = 1e-7
-    loss.free_bits = 0.0
-    # loss.ch1_recons_w = 1
-    # loss.ch2_recons_w = 5
+    loss.free_bits = 1.0
 
     model = config.model
     model.model_type = ModelType.LadderVae
@@ -106,23 +109,33 @@ def get_config():
     model.logvar_lowerbound = -5  # -2.49 is log(1/12), from paper "Re-parametrizing VAE for stablity."
     model.multiscale_lowres_separate_branch = False
     model.multiscale_retain_spatial_dims = True
-    model.monitor = 'val_psnr'  # {'val_loss','val_psnr'}
+    model.monitor = 'val_loss'  # {'val_loss','val_psnr'}
+
+    model.enable_noise_model = True
+    model.noise_model_type = 'gmm'
+    # fname_format = '/home/ashesh.ashesh/training/noise_model/{}/GMMNoiseModel_microscopy-OptiMEM100x014.tif__6_4_Clip0.0-1.0_Sig0.125_UpNone_Norm0_bootstrap.npz'
+    model.noise_model_ch1_fpath = '/home/ashesh.ashesh/training/noise_model/2404/83/GMMNoiseModel_datafiles-Experiment__6_4_Clip0.0-1.0_Sig0.125_UpNone_Norm0_bootstrap.npz'
+    model.noise_model_ch2_fpath = '/home/ashesh.ashesh/training/noise_model/2404/84/GMMNoiseModel_datafiles-Experiment__6_4_Clip0.0-1.0_Sig0.125_UpNone_Norm0_bootstrap.npz'
+
+    model.noise_model_learnable = False
+    # assert model.enable_noise_model == False or model.predict_logvar is None
+
+    # model.noise_model_ch1_fpath = fname_format.format('2307/58', 'actin')
+    # model.noise_model_ch2_fpath = fname_format.format('2307/59', 'mito')
     model.non_stochastic_version = False
-    model.enable_noise_model = False
-    model.noise_model_ch1_fpath = None
-    model.noise_model_ch1_fpath = None
 
     training = config.training
-    training.lr = 0.001 / 2
-    training.lr_scheduler_patience = 30
-    training.max_epochs = 400
-    training.batch_size = 16
+    training.lr = 0.001
+    training.lr_scheduler_patience = 15
+    training.max_epochs = 200
+    training.batch_size = 32
     training.num_workers = 4
     training.val_repeat_factor = None
     training.train_repeat_factor = None
     training.val_fraction = 0.1
     training.test_fraction = 0.1
-    training.earlystop_patience = 200
+    training.earlystop_patience = 100
     training.precision = 16
+    training.limit_train_batches=2000
 
     return config
