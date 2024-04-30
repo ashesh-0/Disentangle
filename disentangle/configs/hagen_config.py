04d0d5bbd0c328700bbe2fa969144bea2aaef4f5
Ashesh <ashesh276@gmail.com>
2024-04-30 15:21:28 +0200

    config update

--- disentangle/configs/hagen_config.py
+++ disentangle/configs/hagen_config.py
@@ -8,7 +8,7 @@ from disentangle.core.sampler_type import SamplerType
 def get_config():
     config = get_default_config()
     data = config.data
-    data.image_size = 64
+    data.image_size = 128
     data.data_type = DataType.SeparateTiffData
     data.channel_1 = 0
     data.channel_2 = 1
@@ -35,7 +35,7 @@ def get_config():
     data.use_one_mu_std = True
     data.train_aug_rotate = True
     data.randomized_channels = False
-    data.multiscale_lowres_count = 3
+    data.multiscale_lowres_count = None
     data.padding_mode = 'reflect'
     data.padding_value = None
     # If this is set to True, then target channels will be normalized from their separate mean.
@@ -49,7 +49,7 @@ def get_config():
     loss.kl_loss_formulation = 'denoisplit_usplit'
     loss.usplit_w = 0.01
     loss.denoisplit_w = 1 - loss.usplit_w
-    loss.restricted_kl = True
+    loss.restricted_kl = False
     loss.reconstruction_weight = 1.0
     # loss.mixed_rec_weight = 1
 
