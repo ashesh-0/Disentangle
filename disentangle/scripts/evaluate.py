df63a3d24e1dae448637152c98c33370634f7fea
Ashesh <ashesh276@gmail.com>
2024-05-17 10:22:35 +0200

    config update

--- disentangle/scripts/evaluate.py
+++ disentangle/scripts/evaluate.py
@@ -245,8 +245,8 @@ def main(
         data_dir = f'{DATA_ROOT}/BioSR/'
     elif dtype == DataType.Derain100H:
         # data_dir = f'{DATA_ROOT}/Rain100H/Rain100HCombined/'
-        data_dir = f'{DATA_ROOT}/Rain100H/Rain100HPractical/tiffs/'
-        # data_dir = f'{DATA_ROOT}/Rain100H/Rain100HCombined_test/'
+        # data_dir = f'{DATA_ROOT}/Rain100H/Rain100HPractical/tiffs/'
+        data_dir = f'{DATA_ROOT}/Rain100H/Rain100HCombined_test/'
         # data_dir = f'{DATA_ROOT}/Rain1000HNew/combined_test/'
     elif dtype == DataType.Dehaze4K:
         data_dir = f'{DATA_ROOT}/Haze4KCombined/test'
@@ -746,7 +746,7 @@ def save_hardcoded_ckpt_evaluations_to_file(normalized_ssim=True,
                                             full_prediction=False,
                                             skip_metrics=False):
     ckpt_dirs = [
-        # '/home/ashesh.ashesh/training/disentangle/2405/D30-M3-S0-L0/37'
+        # '/home/ashesh.ashesh/training/disentangle/2405/D30-M3-S0-L0/28'
         '/home/ashesh.ashesh/training/disentangle/2405/D31-M3-S0-L0/7'
         # '/home/ashesh.ashesh/training/disentangle/2405/D30-M3-S0-L0/28/'
         # '/home/ubuntu.ubuntu/training/disentangle/2403/D16-M23-S0-L0/36',
