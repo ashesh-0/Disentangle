rotation fixed and enabled for training

disentangle/data_loader/multicrops_dset.py
@@ -53,6 +53,8 @@ class MultiCropDset:
         self._rotation_transform = None
         if self._enable_rotation:
             self._rotation_transform = A.Compose([A.Flip(), A.RandomRotate90()])
+        
+        print(f'{self.__class__.__name__} N:{len(self)} Rot:{self._enable_rotation} Ch:{len(self._data_arr)} MaxVal:{self.max_val} Bg:{self._background_values}')
 
 
     def compute_mean_std(self):
@@ -136,20 +138,16 @@ class MultiCropDset:
     def _rotate2D(self, img_tuples):
         img_kwargs = {}
         for i,img in enumerate(img_tuples):
-            for k in range(len(img)):
-                img_kwargs[f'img{i}_{k}'] = img[k]
+            img_kwargs[f'img{i}'] = img
         
         
         keys = list(img_kwargs.keys())
         self._rotation_transform.add_targets({k: 'image' for k in keys})
-        rot_dic = self._rotation_transform(image=img_tuples[0][0], **img_kwargs)
+        rot_dic = self._rotation_transform(image=img_tuples[0], **img_kwargs)
 
         rotated_img_tuples = []
         for i,img in enumerate(img_tuples):
-            if len(img) == 1:
-                rotated_img_tuples.append(rot_dic[f'img{i}_0'][None])
-            else:
-                rotated_img_tuples.append(np.concatenate([rot_dic[f'img{i}_{k}'][None] for k in range(len(img))], axis=0))
+            rotated_img_tuples.append(rot_dic[f'img{i}'])
 
         
         return rotated_img_tuples