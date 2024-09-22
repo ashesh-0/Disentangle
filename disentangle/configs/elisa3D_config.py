0a96ccdc70b2434541548684b0d17c2ca7a95c34
Ashesh <ashesh276@gmail.com>
2024-09-22 21:16:43 +0200

    config update

--- disentangle/configs/elisa3D_config.py
+++ disentangle/configs/elisa3D_config.py
@@ -17,7 +17,7 @@ def get_config():
     data.channel_idx_list = [0,1]
     data.zstart = 25
     data.zstop = 40
-    data.depth3D = 5
+    data.depth3D = 9
     data.mode_3D = True
     assert data.depth3D <= data.zstop - data.zstart
 
@@ -43,7 +43,7 @@ def get_config():
     data.use_one_mu_std = True
     data.train_aug_rotate = True
     data.randomized_channels = False
-    data.multiscale_lowres_count = 3
+    data.multiscale_lowres_count = None
     data.padding_mode = 'reflect'
     data.padding_value = None
     # If this is set to True, then target channels will be normalized from their separate mean.
@@ -128,7 +128,7 @@ def get_config():
     training.lr = 0.001
     training.lr_scheduler_patience = 30
     training.max_epochs = 400
-    training.batch_size = 32
+    training.batch_size = 16
     training.num_workers = 4
     training.val_repeat_factor = None
     training.train_repeat_factor = None
