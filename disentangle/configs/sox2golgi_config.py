3ee44222842f9b2a601647e68ca35836a53e43d2
Ashesh <ashesh276@gmail.com>
2025-03-18 15:52:06 +0100

    config update

--- disentangle/configs/sox2golgi_config.py
+++ disentangle/configs/sox2golgi_config.py
@@ -14,7 +14,7 @@ from disentangle.data_loader.sox2golgi_rawdata_loader import Sox2GolgiChannelLis
 def get_config():
     config = get_default_config()
     data = config.data
-    data.image_size = 64
+    data.image_size = 128
     data.data_type = DataType.TavernaSox2Golgi
     data.subdset_type = SubDsetType.TwoChannel
     data.channel_1 = Sox2GolgiChannelList.Sox2
@@ -42,7 +42,7 @@ def get_config():
     data.use_one_mu_std = True
     data.train_aug_rotate = True
     data.randomized_channels = False
-    data.multiscale_lowres_count = 3
+    data.multiscale_lowres_count = None
     data.padding_mode = 'reflect'
     data.padding_value = None
     # If this is set to True, then target channels will be normalized from their separate mean.
@@ -58,7 +58,7 @@ def get_config():
     loss = config.loss
     loss.loss_type = LossType.DenoiSplitMuSplit
     # this is not uSplit.
-    loss.kl_loss_formulation = 'denoisplit_usplit'
+    loss.kl_loss_formulation = 'usplit'
     loss.restricted_kl = True
 
     # loss.mixed_rec_weight = 1
@@ -115,7 +115,7 @@ def get_config():
     model.multiscale_retain_spatial_dims = True
     model.monitor = 'val_loss'  # {'val_loss','val_psnr'}
 
-    model.enable_noise_model = True
+    model.enable_noise_model = False
     model.noise_model_type = 'gmm'
     model.noise_model_ch1_fpath = '/home/ashesh.ashesh/training/noise_model/2404/116/GMMNoiseModel_SOX2-C2__6_4_Clip0.0-1.0_Sig0.125_UpNone_Norm0_bootstrap.npz'
     model.noise_model_ch2_fpath = '/home/ashesh.ashesh/training/noise_model/2404/115/GMMNoiseModel_GOLGI-C1__6_4_Clip0.0-1.0_Sig0.125_UpNone_Norm0_bootstrap.npz'
