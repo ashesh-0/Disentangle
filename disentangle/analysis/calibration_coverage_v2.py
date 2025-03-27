cba8aa8fa0bc36d1b17582fffda0454b432fad17
Ashesh <ashesh276@gmail.com>
2025-03-27 15:42:26 +0100

    noteook update

--- disentangle/analysis/calibration_coverage_v2.py
+++ disentangle/analysis/calibration_coverage_v2.py
@@ -14,8 +14,24 @@ For every patch (or full frame, or every pixel):
 import numpy as np
 import scipy
 import torch
+from scipy import stats
 from torch.utils.data import DataLoader
 from tqdm import tqdm
+
+
+def find_optimal_scalar_offset(pred, gt):
+    """
+    Optimal linear transform which minimizes the MSE between pred and gt.
+
+    Start by making them zero mean. 
+    This yields a simple formulation of the optimal scalar: covariance(pred,gt)/var(pred)
+    then the offset is: mean(gt) - scalar * mean(pred)
+    """
+    pred_zero_mean = pred - np.mean(pred)
+    gt_zero_mean = gt - np.mean(gt)
+    scalar = np.sum(pred_zero_mean * gt_zero_mean) / np.sum(pred_zero_mean ** 2)
+    offset = np.mean(gt) - scalar * np.mean(pred)
+    return scalar, offset
 
 
 def get_empirical_coverage_and_confidence_level(coverage_data):
@@ -34,8 +50,15 @@ def get_empirical_coverage_and_confidence_level(coverage_data):
     return confidence_level, empirical_coverage
 
 
-def compute_for_one(patch_predictions, gt, percentile_bins = 100, elem_size = 10, calib_stats_dict=None):
+def compute_for_one(patch_predictions, gt, percentile_bins = 100, elem_size = 10, calib_stats_dict=None, background_patch_detection_func=None):
     output = []
+
+    masks = []
+    if background_patch_detection_func is not None:
+        for ch_idx in range(gt.shape[1]):
+            bkg_mask = background_patch_detection_func(gt[:, ch_idx], ch_idx)
+            masks.append(bkg_mask)
+
     for ch_idx in range(gt.shape[1]):
         if calib_stats_dict is not None:
             std_factor = calib_stats_dict[ch_idx]['scalar']
@@ -54,13 +77,17 @@ def compute_for_one(patch_predictions, gt, percentile_bins = 100, elem_size = 10
                                               gt[:, ch_idx,h:h+elem_size,w:w+elem_size], 
                                               percentile_bins=percentile_bins,
                                               std_factor=std_factor, std_offset=std_offset)
+                # print('see the shape', val.shape, gt.shape, masks[ch_idx].mean())
+                val[masks[ch_idx]] = np.nan
+
                 computed_vals.append(val)
             output.append(np.concatenate(computed_vals, axis=0))
         else:
             output.append(compute_for_one_channel(patch_predictions[:, :, ch_idx], gt[:, ch_idx], 
                                               percentile_bins=percentile_bins,
                                               std_factor=std_factor, std_offset=std_offset))
-    return np.stack(output, axis=1)
+    output = np.stack(output, axis=1)
+    return output
 
 def compute_for_one_channel(patch_predictions, gt, percentile_bins = 100, std_factor = 1, std_offset = 0):
     """
@@ -90,14 +117,14 @@ def compute_for_one_channel(patch_predictions, gt, percentile_bins = 100, std_fa
         # quantiles = np.percentile(var_sample, np.linspace(0, 100, 100))
         # percentile = np.searchsorted(quantiles, mse_mmse_sample)
 
-        sym_percentile = np.abs(percentile - 50)
+        # sym_percentile = np.abs(percentile - 50)
         # print(var_sample.shape, mse_mmse_sample.shape, quantiles.shape, quantile.shape)
         # raise ValueError('stop')
         true_error_symmetric_percentiles.append(sym_percentile)
     return np.array(true_error_symmetric_percentiles)
 
 
-def get_calibration_coverage_data(model, dset, percentile_bins = 100, num_workers=4, batch_size = 32, mmse_count = 10, elem_size=10, calib_stats_dict=None):
+def get_calibration_coverage_data(model, dset, percentile_bins = 100, num_workers=4, batch_size = 32, mmse_count = 10, elem_size=10, calib_stats_dict=None, background_patch_detection_func=None):
     dloader = DataLoader(dset, pin_memory=False, num_workers=num_workers, shuffle=False, batch_size=batch_size)
     q_values_dataset = []
     with torch.no_grad():
@@ -115,8 +142,83 @@ def get_calibration_coverage_data(model, dset, percentile_bins = 100, num_worker
                 recon_img_list.append(pred_imgs.cpu().numpy()[:,None])
 
             samples = np.concatenate(recon_img_list, axis=1)
-            true_error_quantiles = compute_for_one(samples, tar_normalized.cpu().numpy(), percentile_bins=percentile_bins, elem_size=elem_size, calib_stats_dict=calib_stats_dict)
+            true_error_quantiles = compute_for_one(samples, tar_normalized.cpu().numpy(), percentile_bins=percentile_bins, elem_size=elem_size, calib_stats_dict=calib_stats_dict, background_patch_detection_func=background_patch_detection_func)
             q_values_dataset.append(true_error_quantiles)
 
     return np.concatenate(q_values_dataset, axis=0)
 
+
+# ===========================================================
+# finding the optimal transform for the calibration coverage.
+# ===========================================================
+def divide_into_smaller_patches(np_array, elem_size=10):
+    """
+    np_array: Batch X C x H x W
+    """
+    batch_size, C, H, W = np_array.shape
+    cropH = H % elem_size
+    cropW = W % elem_size
+    # crop center
+    if cropH != 0:
+        np_array = np_array[..., cropH//2:-cropH//2,:]
+    if cropW != 0:
+        np_array = np_array[..., cropW//2:-cropW//2]
+
+    nH = H // elem_size
+    nW = W // elem_size
+    np_array = np_array.reshape(batch_size, C, nH, elem_size, nW, elem_size)
+    np_array = np.transpose(np_array, axes=(0, 2, 4, 1, 3, 5))
+    np_array = np_array.reshape(-1, C, elem_size, elem_size)
+    return np_array
+
+
+def patches_for_optimal_transform(model, dset,num_workers=4, batch_size = 32, mmse_count = 10, elem_size=10, skip_pixels=0):
+    dloader = DataLoader(dset, pin_memory=False, num_workers=num_workers, shuffle=False, batch_size=batch_size)
+    tar_patches = []
+    recons_patches = []
+    skip = skip_pixels//2
+
+    with torch.no_grad():
+        for batch in tqdm(dloader):
+            inp, tar = batch[:2]
+            inp = inp.cuda()
+            tar = tar.cuda()
+
+            x_normalized = model.normalize_input(inp)
+            tar_normalized = model.normalize_target(tar)
+            _, nC, nH, nW = tar.shape
+            recon_img_list = []
+            for _ in range(mmse_count):
+                pred_imgs, _ = model(x_normalized)
+                pred_imgs = pred_imgs[:,:nC, skip:nH-skip, skip:nW-skip].cpu().numpy()
+                if elem_size is not None:
+                    pred_imgs = divide_into_smaller_patches(pred_imgs, elem_size)
+
+                recon_img_list.append(pred_imgs[:,None])
+            
+            tar_normalized = tar_normalized.cpu().numpy()[..., skip:nH-skip, skip:nW-skip]
+            if elem_size is not None:
+                tar_normalized = divide_into_smaller_patches(tar_normalized, elem_size)
+            recons_patches.append(np.concatenate(recon_img_list, axis=1))
+            tar_patches.append(tar_normalized)
+    tar_patches = np.concatenate(tar_patches, axis=0)
+    recons_patches = np.concatenate(recons_patches, axis=0)
+    return tar_patches, recons_patches
+
+def fit_optimal_transform(recons_patches, tar_patches):
+    mmse_recons = recons_patches.mean(axis=1)
+    # N x C x H x W for both tar_patches and mmse_recons
+    assert tar_patches.shape == mmse_recons.shape, f'{tar_patches.shape} != {mmse_recons.shape}'
+    assert tar_patches.ndim == 4, f'{tar_patches.ndim} != 4'
+    
+    sample_var = ((mmse_recons[:,None] - recons_patches) ** 2).mean(axis=(-2,-1))
+    sample_std = np.sqrt(sample_var)
+    avg_sample_std = sample_std.mean(axis=1)
+
+    rmse = ((mmse_recons - tar_patches) ** 2).mean(axis=(-2,-1))
+    rmse = np.sqrt(rmse)
+    output = {}
+    for ch_idx in range(tar_patches.shape[1]):
+        slope, intercept, *_ = stats.linregress(avg_sample_std[:,ch_idx],rmse[:,ch_idx])
+        output[ch_idx] = {'scalar':slope, 'offset':intercept}
+    return output
---
+++ disentangle/loss/ssl_finetuning.py
@@ -0,0 +1,91 @@
+
+import torch
+from torch.utils.data import DataLoader
+from tqdm import tqdm
+
+from finetunesplit.loss import SSL_loss
+
+
+def finetune_two_forward_passes(model, val_dset, transform_obj, max_step_count=10000, batch_size=16, skip_pixels=0,
+                                scalar_params_dict=None,
+                                optimization_params_dict=None):
+    
+    # enable dropout.
+    model.train()
+
+    def pred_func(inp):
+        return model(inp)[0][:,:2]
+
+    factor1 = scalar_params_dict.get('factor1', None)
+    offset1 = scalar_params_dict.get('offset1', None)
+    factor2 = scalar_params_dict.get('factor2', None)
+    offset2 = scalar_params_dict.get('offset2', None)
+    mixing_ratio = scalar_params_dict.get('mixing_ratio', None)
+                                        
+    # define a learnable scalar and an offset 
+    assert factor1 is not None
+    assert offset1 is not None
+    assert factor2 is not None
+    assert offset2 is not None
+    assert optimization_params_dict is not None, "Please provide optimization parameters"
+    # if factor1 is None:
+    #     factor1 = torch.nn.Parameter(torch.tensor(1.0).cuda())
+
+    # if offset1 is None:
+    #     offset1 = torch.nn.Parameter(torch.tensor(0.0).cuda())
+
+    # if factor2 is None:
+    #     factor2 = torch.nn.Parameter(torch.tensor(1.0).cuda())
+    # if offset2 is None:
+    #     offset2 = torch.nn.Parameter(torch.tensor(0.0).cuda())
+    
+    # define an optimizer
+    # opt = torch.optim.Adam(model.parameters(), lr=1e-3)
+    opt = torch.optim.Adamax(optimization_params_dict['parameters'], lr=optimization_params_dict['lr'], weight_decay=0)
+
+    loss_arr = []
+    best_loss = 1e6
+
+    factor1_arr = []
+    offset1_arr = []
+
+    factor2_arr = []
+    offset2_arr = []
+    mixing_ratio_arr= []
+    best_factors = best_offsets = None
+
+    cnt = 0
+    while True:
+        dloader = DataLoader(val_dset, batch_size=batch_size, shuffle=True, num_workers=4)
+        for i, (inp, tar) in tqdm(enumerate(dloader)):
+            if i >= max_step_count:
+                break
+            inp = inp.cuda()
+            # reset the gradients
+            opt.zero_grad()
+
+            loss_dict = SSL_loss(pred_func, inp, transform_obj, mixing_ratio=mixing_ratio, factor1=factor1, offset1=offset1, factor2=factor2, offset2=offset2, skip_pixels=skip_pixels)
+            loss = loss_dict['loss_inp']
+            loss.backward()
+            loss_arr.append(loss.item())
+            
+            factor1_arr.append(factor1.item())
+            offset1_arr.append(offset1.item())
+            factor2_arr.append(factor2.item())
+            offset2_arr.append(offset2.item())
+            cnt += len(inp)
+            mixing_ratio_arr.append(mixing_ratio.item())
+            opt.step()
+            if loss.item() < best_loss:
+                best_loss = loss.item()
+                print(f'Loss: {loss.item():.2f}')
+                best_factors = [factor1.item(), factor2.item()]
+                best_offsets = [offset1.item(), offset2.item()]
+            # print(f'Loss: {loss.item():.2f}')
+            if cnt >= max_step_count:
+                break
+        if cnt >= max_step_count:
+            break
+    
+    model.eval()
+    return {'loss': loss_arr, 'best_loss': best_loss, 'best_factors': best_factors, 'best_offsets': best_offsets, 'factor1': factor1_arr, 'offset1': offset1_arr, 'factor2': factor2_arr, 'offset2': offset2_arr, 'mixing_ratio': mixing_ratio_arr}
