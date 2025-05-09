222890cb3de41779a274e0abb1c074d5ea719362
Ashesh <ashesh276@gmail.com>
2025-05-09 16:10:32 +0200

    hagenetal baseline working

--- disentangle/data_loader/indisplit_rawdata_loader.py
+++ disentangle/data_loader/indisplit_rawdata_loader.py
@@ -1,5 +1,7 @@
 import os
 
+import numpy as np
+
 from disentangle.core.data_split_type import DataSplitType
 from disentangle.core.data_type import DataType
 from disentangle.core.tiff_reader import load_tiff
@@ -11,7 +13,7 @@ def get_train_val_data(datadir,
             val_fraction=None,
             test_fraction=None):
     
-    if data_config.data_type in [DataType.indiSplit_BioSR, DataType.indiSplit_HTT24, DataType.indiSplit_HTLIF24, DataType.indiSplit_PaviaATN, DataType.indiSplit_HagenEtAl]:
+    if data_config.data_type in [DataType.indiSplit_BioSR, DataType.indiSplit_HTT24, DataType.indiSplit_HTLIF24, DataType.indiSplit_PaviaATN]:
         if datasplit_type == DataSplitType.Train:
             datadir = os.path.join(datadir, 'train')
         elif datasplit_type == DataSplitType.Val:
@@ -32,4 +34,18 @@ def get_train_val_data(datadir,
             data = data[...,:2]
 
         print(f'Loaded {DataType.name(data_config.data_type)} data from {fpaths[0]}', data.shape)
+        return data
+    elif data_config.data_type == DataType.indiSplit_HagenEtAl:
+        fpath1 = 'train/train_actin-60x-noise2-highsnr.tif'
+        fpath2 = 'train/train_mito-60x-noise2-highsnr.tif'
+        if datasplit_type == DataSplitType.Val:
+            fpath1 = fpath1.replace('train', 'val')
+            fpath2 = fpath2.replace('train', 'val')
+        elif datasplit_type == DataSplitType.Test:
+            fpath1 = fpath1.replace('train', 'test')
+            fpath2 = fpath2.replace('train', 'test')
+        data1 = load_tiff(os.path.join(datadir, fpath1))
+        data2 = load_tiff(os.path.join(datadir, fpath2))
+        data = np.stack([data1, data2], axis=-1)
+        print(f'Loaded {DataType.name(data_config.data_type)} data from {fpath1} and {fpath2}', data.shape)
         return data
