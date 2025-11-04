e1657652b227e012c88e75f89b4744a54c9952a2
Ashesh <ashesh276@gmail.com>
2025-11-04 12:31:18 +0100

    config update

--- disentangle/configs/htt24_denoisplit.py
+++ disentangle/configs/htt24_denoisplit.py
@@ -134,7 +134,7 @@ def get_config():
 
     # model.noise_model_ch1_fpath = fname_format.format('2307/58', 'actin')
     # model.noise_model_ch2_fpath = fname_format.format('2307/59', 'mito')
-    model.non_stochastic_version = True
+    model.non_stochastic_version = False
 
     training = config.training
     training.lr = 0.001
